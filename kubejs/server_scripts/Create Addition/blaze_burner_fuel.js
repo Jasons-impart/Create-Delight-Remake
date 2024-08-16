c50fef54d6e4b6436756a21cf1a8e4477a841424
JasonQ1123 <nmuqindexiang@163.com>
2024-08-16 17:49:48 +0800

    移除cca吸管

--- kubejs/server_scripts/Create Addition/blaze_burner_fuel.js
+++ kubejs/server_scripts/Create Addition/blaze_burner_fuel.js
@@ -1,31 +1,4 @@
 ServerEvents.recipes(e => {
-    // 添加燃油和凌冰燃油成为烈焰人燃烧室的燃料
-    e.custom({
-        "type": "createaddition:liquid_burning",
-        "input": {
-            "fluid": "ad_astra:fuel",
-            "amount": 1000
-        },
-        "burnTime": 80000,
-        "superheated": true
-    })
-    e.custom({
-        "type": "createaddition:liquid_burning",
-        "input": {
-            "fluid": "ad_astra:cryo_fuel",
-            "amount": 1000
-        },
-        "burnTime": 36000,
-        "superheated": true
-    })
-    // 添加混合燃料为烈焰人燃烧室的燃料
-    e.custom({
-        "type": "createaddition:liquid_burning",
-        "input": {
-            "fluid": 'createdelight:fuel_mixtures',
-            "amount": 1000
-        },
-        "burnTime": 72000,
-        "superheated": true
-    })
+    e.remove({type:"createaddition:liquid_burning"})
+    e.remove({id: "createaddition:rolling/straw"})
 })
