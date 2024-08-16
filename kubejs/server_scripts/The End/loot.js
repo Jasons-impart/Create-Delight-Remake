158880798f629c3307dfa18ed69aa79d545f3665
JasonQ1123 <nmuqindexiang@163.com>
2024-08-16 17:17:05 +0800

    超多更改

--- kubejs/server_scripts/The End/loot.js
+++ kubejs/server_scripts/The End/loot.js
@@ -1,7 +1,7 @@
 LootJS.modifiers(e => {
     // 末影龙掉落龙肉
     e.addEntityLootModifier("minecraft:ender_dragon")
-        .addWeightedLoot([1, 8], [
+        .addWeightedLoot([3, 8], [
             Item.of("ends_delight:raw_dragon_meat")
                 .withChance(30),
             Item.of("ends_delight:dragon_leg")
