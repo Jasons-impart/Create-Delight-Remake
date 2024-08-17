4298ac366afb02396b600eb4a594316dc0255e62
JasonQ1123 <nmuqindexiang@163.com>
2024-08-17 20:14:18 +0800

    spring_tags兼容

--- kubejs/server_scripts/Create vintage improvements/recipes.js
+++ kubejs/server_scripts/Create vintage improvements/recipes.js
@@ -1,3 +1,95 @@
 ServerEvents.recipes(e => {
+    e.recipes.kubejs.shaped(
+        "vintageimprovements:vibrating_table", [
+            "ABA",
+            "ACA"
+        ], {
+            A: "#forge:spring/between_500_2_1000",
+            B: "#minecraft:wooden_slabs",
+            C: "create:mechanical_piston"
+        }
+    ).id("vintageimprovements:craft/vibrating_table")
+    e.recipes.kubejs.shapeless(
+        "create:weighted_ejector",
+        [
+            "#forge:spring/between_500_2_1000",
+            "create:depot",
+            "create:cogwheel"
+        ]
+    ).id("create:crafting/kinetics/weighted_ejector")
+    e.recipes.kubejs.shapeless(
+        "3x create:portable_storage_interface",
+        [
+            "#forge:spring/blow_500",
+            "create:andesite_casing",
+            "create:chute"
+        ]
+    ).id("create:crafting/kinetics/portable_storage_interface")
+    e.recipes.kubejs.shapeless(
+        "create:portable_fluid_interface",
+        [
+            "#forge:spring/blow_500",
+            "create:copper_casing",
+            "create:chute"
+        ]
+    ).id("create:crafting/kinetics/portable_fluid_interface")
+    e.recipes.kubejs.shaped(
+        "create:spout", [
+            "ABA",
+            " C "
+        ], {
+            A: "#forge:spring/blow_500",
+            B: "create:copper_casing",
+            C: "minecraft:dried_kelp"
+        }
+    ).id("create:crafting/kinetics/spout")
+    e.recipes.kubejs.shaped(
+        "create_enchantment_industry:printer", [
+            "ABA",
+            " C ",
+            " D "
+        ], {
+            A: "#forge:spring/blow_500",
+            B: "create:copper_casing",
+            C: "minecraft:dried_kelp",
+            D: "#createbigcannons:sheet_iron"
+        }
+    ).id("create_enchantment_industry:crafting/printer")
     
+    e.recipes.kubejs.shaped(
+        "3x create:steam_engine", [
+            " A ",
+            " B ",
+            "DC "
+        ], {
+            A: "create:shaft",
+            B: "#forge:spring/between_500_2_1000",
+            C: "minecraft:copper_block",
+            D: "create_sa:steam_engine"
+        }
+    ).id("create:crafting/kinetics/steam_engine")
+    e.recipes.kubejs.shaped(
+        "create:steam_whistle", [
+            " A ",
+            " B ",
+            " C "
+        ], {
+            A: "create:golden_sheet",
+            B: "#forge:spring/blow_500",
+            C: "minecraft:copper_ingot"
+        }
+    ).id("create:crafting/kinetics/steam_whistle")
+    e.recipes.kubejs.shaped(
+        "create:mechanical_arm", [
+            "AAB",
+            "AC ",
+            "DE "
+        ], {
+            A: "create:brass_sheet",
+            B: "create:andesite_alloy",
+            C: "#forge:spring/between_500_2_1000",
+            D: "create:precision_mechanism",
+            E: "create:brass_casing"
+        }
+    ).id("create:crafting/kinetics/mechanical_arm")
 })
---
+++ kubejs/server_scripts/Create vintage improvements/spring_tags.js
@@ -0,0 +1,26 @@
+ServerEvents.tags("item", e => {
+    e.add("forge:spring/blow_500", [
+        'vintageimprovements:tin_spring',
+        'vintageimprovements:golden_spring',
+        'vintageimprovements:electrum_spring',
+        'vintageimprovements:brass_spring',
+        'vintageimprovements:bronze_spring',
+        'vintageimprovements:andesite_spring',
+        'vintageimprovements:zinc_spring',
+        'vintageimprovements:cast_iron_spring',
+        'vintageimprovements:copper_spring',
+        'vintageimprovements:vanadium_spring'
+    ])
+    e.add("forge:spring/between_500_2_1000", [
+        'vintageimprovements:iron_spring',
+        'vintageimprovements:steel_spring',
+        'vintageimprovements:blaze_spring',
+        'vintageimprovements:desh_spring',
+        'vintageimprovements:ostrum_spring'
+    ])
+    e.add("forge:spring/over_1000", [
+        'vintageimprovements:nethersteel_spring',
+        'vintageimprovements:calorite_spring',
+        'vintageimprovements:netherite_spring'
+    ])
+})
