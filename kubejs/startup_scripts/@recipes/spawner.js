d6babc238ecd2a80dfd24981b6278fe2275476fc
JasonQ1123 <nmuqindexiang@163.com>
2024-09-08 00:42:35 +0800

    添加动力刷怪笼相关配方

---
+++ kubejs/startup_scripts/@recipes/spawner.js
@@ -0,0 +1,4 @@
+new Schema("create_mechanical_spawner:spawner")
+    .simpleKey("fluid", "inputFluid")
+    .simpleKey("mob", "entityTypeTag")
+    .simpleKey("processingTime", "doubleNumber", 2500)
