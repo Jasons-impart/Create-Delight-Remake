6c2dead120fd21029c11482d0f1ee576e16fd84c
JasonQ1123 <nmuqindexiang@163.com>
2024-09-08 15:03:06 +0800

    更新提示文本

--- kubejs/client_scripts/bug_fix.js
+++ kubejs/client_scripts/bug_fix.js
@@ -1,6 +1,7 @@
 BlockEvents.rightClicked("alexsmobs:capsid", e => {
     const { block, player, level } = e
     if( block.entityData["Items"][0]["id"] == "create:minecart_contraption" && player.mainHandItem.id == "create:minecart_contraption"){
+        player.tell(Text.translate("message.createdelight.capsid"))
         e.cancel()
         player.sendInventoryUpdate()
     }
