92599d98103434324fc1931e22ec1f25f134f59e
JasonQ1123 <nmuqindexiang@163.com>
2024-09-09 22:52:56 +0800

    增加食物修改轮子

--- kubejs/startup_scripts/modifier_item.js
+++ kubejs/startup_scripts/modifier_item.js
@@ -2,46 +2,33 @@ ItemEvents.modification(e => {
     e.modify("deeperdarker:heart_of_the_deep", item => {
         item.maxStackSize = 64
     })
-    e.modify("vintagedelight:surstromming", item => {
-        item.foodProperties = food => {
-            food.effect("minecraft:nausea", 1200, 2, 1.0)
-        }
+    // 饱和度计算公式为hunger*saturation*2
+    let hungers = [
+        ["vintagedelight:cheese_pizza_slice", 5, 0.7],
+    ]
+    hungers.forEach(hunger => {
+        e.modify(hunger[0], item => {
+            item.foodProperties = food => {
+                food.hunger(hunger[1])
+                food.saturation(hunger[2])
+            }
+        });
     })
-    e.modify("vintagedelight:cheese_pizza_slice", item => {
-        item.foodProperties = food => {
-            food.hunger(5)
-            food.saturation(0.7)
-        }
-    })
-    e.modify("culturaldelights:squid", item => {
-        item.foodProperties = food => {
-            food.effect("minecraft:darkness", 120, 1, 1.0)
-        }
-    })
-    e.modify("culturaldelights:glow_squid", item => {
-        item.foodProperties = food => {
-            food.effect("minecraft:glowing", 120, 1, 1.0)
-        }
-    })
-    e.modify("mynethersdelight:ghast_dough", item => {
-        item.foodProperties = food => {
-            food.effect("minecraft:levitation", 120, 1, 1)
-        }
-    })
-    e.modify("culturaldelights:pickle", item => {
-        item.foodProperties = food => {
-            food.effect("farmersdelight:nourishment", 600, 1, 1)
-        }
-    })
-    e.modify("culturaldelights:cut_pickle", item => {
-        item.foodProperties = food => {
-            food.effect("farmersdelight:nourishment", 300, 1, 1)
-        }
-    })
-    e.modify("culturaldelights:cut_pickle", item => {
-        item.foodProperties = food => {
-            food.effect("farmersrespite:caffeinated", 200, 1, 1)
-            food.effect("minecraft:hunger", 120, 1, 1)
-        }
-    })
+    // 参数含义分别为，食物id，effectid，持续时间（tick），强度，获得效果的概率
+    let effects = [
+        ["vintagedelight:surstromming", "minecraft:nausea", 1200, 2, 1.0],
+        ["culturaldelights:squid", "minecraft:darkness", 120, 1, 1.0],
+        ["culturaldelights:glow_squid", "minecraft:glowing", 120, 1, 1.0],
+        ["mynethersdelight:ghast_dough", "minecraft:levitation", 120, 1, 1.0],
+        ["culturaldelights:pickle", "farmersdelight:nourishment", 600, 1, 1.0],
+        ["culturaldelights:cut_pickle", "farmersdelight:nourishment", 300, 1, 1],
+
+    ]
+    effects.forEach(effect => {
+        e.modify(effect[0], item => {
+            item.foodProperties = food => {
+                food.effect(effect[1], effect[2], effect[3], effect[4])
+            }
+        })
+    });
 })
