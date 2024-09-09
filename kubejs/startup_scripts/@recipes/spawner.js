8b6706160a66c8408592aad9092afd1e1e7e989d
JasonQ1123 <nmuqindexiang@163.com>
2024-09-09 16:25:02 +0800

    翻译

--- kubejs/startup_scripts/@recipes/spawner.js
+++ kubejs/startup_scripts/@recipes/spawner.js
@@ -1,4 +1,4 @@
-new Schema("create_mechanical_spawner:spawner")
-    .simpleKey("fluid", "inputFluid")
-    .simpleKey("mob", "entityTypeTag")
-    .simpleKey("processingTime", "doubleNumber", 2500)
+// new Schema("create_mechanical_spawner:spawner")
+//     .simpleKey("fluid", "inputFluid")
+//     .simpleKey("mob", "entityTypeTag")
+//     .simpleKey("processingTime", "doubleNumber", 2500)
