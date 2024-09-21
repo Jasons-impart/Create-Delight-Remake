b5a14e3ef10cd69bd032a07e4ff76b347e1719c3
JasonQ1123 <nmuqindexiang@163.com>
2024-09-21 23:20:13 +0800

    基础金属矿物全部实现兼容冶金学

---
+++ kubejs/startup_scripts/custom/register_metal_dust.js
@@ -0,0 +1,19 @@
+StartupEvents.registry("item", e => {
+    let metals = [
+        "tin",
+        "silver",
+        "desh",
+        "ostrum",
+        "calorite"
+    ]
+    metals.forEach(metal => {
+        e.create(`createdelight:${metal}_dust`)
+            .maxStackSize(64)
+            .translationKey(`item.createdelight.${metal}_dust`)
+            .tag(`forge:dusts/${metal}`)
+        e.create(`createdelight:dirty_${metal}_dust`)
+            .maxStackSize(64)
+            .translationKey(`item.createdelight.dirty_${metal}_dust`)
+            .tag(`forge:dirty_dusts/dirty_${metal}`)
+    })
+})
