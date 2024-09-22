9a677c64fa670a40ee694556a56d8057fb61a681
SSWTLZZ <987508451@qq.com>
2024-09-22 13:13:58 +0800

    主世界不再生成悚怖怪

---
+++ kubejs/server_scripts/Ice and Fire/prevent_summon.js
@@ -0,0 +1,17 @@
+EntityEvents.spawned(e => {
+    let dread_monster = [
+        "iceandfire:dread_beast", 
+        "iceandfire:dread_ghoul", 
+        "iceandfire:dread_horse",
+        "iceandfire:dread_knight",
+        "iceandfire:dread_lich",
+        "iceandfire:dread_lich_skull",
+        "iceandfire:dread_scuttler",
+        "iceandfire:dread_thrall"]
+    // 苍蝇生成
+    if (dread_monster.find(e.entity.type)) {
+        if (e.level.dimension == "minecraft:overworld") {
+            e.cancel()
+        }
+    }
+})
