9e5e6840113b416bbabea7f10f143e63b9eb3204
JasonQ1123 <nmuqindexiang@163.com>
2024-10-05 22:32:37 +0800

    任务修复及bug修复

--- kubejs/server_scripts/Create/bug_fix.js
+++ kubejs/server_scripts/Create/bug_fix.js
@@ -3,4 +3,12 @@ BlockEvents.rightClicked("alexsmobs:capsid", e => {
     if( block.entityData["Items"][0]["id"] == "create:minecart_contraption" && player.mainHandItem.id == "create:minecart_contraption"){
         e.cancel()
     }
+    if( block.entityData["Items"][0]["id"].search("present") != -1 && player.mainHandItem.id.search("present") != -1 ){
+        e.cancel()
+    }
+})
+ServerEvents.tags("block", e => {
+    e.remove("create:safe_nbt", [
+        "create:clipboard"
+    ])
 })
