a69d10cafcaf02811cc03acd7f40136a11a7f1c0
SSWTLZZ <987508451@qq.com>
2024-10-11 12:07:41 +0800

    继续完善食物悬赏

---
+++ kubejs/server_scripts/Create Cafe/tags.js
@@ -0,0 +1,27 @@
+ServerEvents.tags("minecraft:item", e => {
+    e.add("createdelight:cafe", [
+        'createcafe:iced_coffee',
+        'createcafe:iced_coffee_milk',
+        'createcafe:caramel_iced_coffee',
+        'createcafe:banana_iced_coffee'])
+    e.add("createdelight:milk_tea", [
+        'createcafe:pineapple_milk_tea',
+        'createcafe:blueberry_milk_tea',
+        'createcafe:lychee_milk_tea',
+        'createcafe:grape_milk_tea',
+        'createcafe:fig_milk_tea',
+        'createcafe:durian_milk_tea',
+        'createcafe:peach_milk_tea',
+        'createcafe:mango_milk_tea',
+        'createcafe:banana_milk_tea',
+        'createcafe:watermelon_milk_tea',
+        'createcafe:lemon_milk_tea',
+        'createcafe:persimmon_milk_tea',
+        'createcafe:orange_milk_tea',
+        'createcafe:kiwi_milk_tea',
+        'createcafe:sweetberry_milk_tea',
+        'createcafe:pumpkin_milk_tea',
+        'createcafe:apple_milk_tea',
+        'createcafe:cherry_milk_tea',
+        'createcafe:oreo_milk_tea'])
+})
