704d7b571745f79ee22d60c3572ebfb7c07ce6a6
SSWTLZZ <987508451@qq.com>
2024-10-12 23:24:13 +0800

    离心机bug拿hotai修好了

---
+++ kubejs/server_scripts/Create Metallurgy/recipe.js
@@ -0,0 +1,3 @@
+ServerEvents.recipes(e => {
+    metal_production_line_5(e, ["createmetallurgy:dirty_copper_dust", "createmetallurgy:copper_dust", "create:crushed_raw_copper", "minecraft:raw_copper", "create:copper_nugget"])
+})
