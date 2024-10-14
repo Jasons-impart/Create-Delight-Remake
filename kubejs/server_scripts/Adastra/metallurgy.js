8aea280160f0144d810633479849878af5a82c37
SSWTLZZ <987508451@qq.com>
2024-10-14 18:55:59 +0800

    矿簇物品加需求应力tooltip，冶金学再再完善

--- kubejs/server_scripts/Adastra/metallurgy.js
+++ kubejs/server_scripts/Adastra/metallurgy.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes(e => {
-    e.replaceOutput({type: "create:crushing", output: "ad_astra:raw_desh" }, "ad_astra:raw_desh", "createdelight:crushed_raw_desh")
-    e.replaceOutput({type: "create:crushing", output: "ad_astra:raw_ostrum" }, "ad_astra:raw_ostrum", "createdelight:crushed_raw_ostrum")
-    e.replaceOutput({type: "create:crushing", output: "ad_astra:raw_calorite" }, "ad_astra:raw_calorite", "createdelight:crushed_raw_calorite")
+    const { createmetallurgy } = e.recipes
+    e.replaceOutput({ type: "create:crushing", output: "ad_astra:raw_desh" }, "ad_astra:raw_desh", "createdelight:crushed_raw_desh")
+    e.replaceOutput({ type: "create:crushing", output: "ad_astra:raw_ostrum" }, "ad_astra:raw_ostrum", "createdelight:crushed_raw_ostrum")
+    e.replaceOutput({ type: "create:crushing", output: "ad_astra:raw_calorite" }, "ad_astra:raw_calorite", "createdelight:crushed_raw_calorite")
     metal_production_line_6(e, [
         "ad_astra:desh_block",
         "ad_astra:desh_ingot",
@@ -32,5 +33,23 @@ ServerEvents.recipes(e => {
         "createdelight:molten_calorite"],
         "heated",
         80)
-    
+    createmetallurgy.melting(Fluid.of("createdelight:molten_desh", 90), "createdelight:dirty_desh_dust")
+        .heatRequirement("heated")
+        .id("createmetallurgy:melting/molten_desh_from_dirty_desh_dust")
+    createmetallurgy.melting(Fluid.of("createdelight:molten_desh", 90), "createdelight:desh_dust")
+        .heatRequirement("heated")
+        .id("createmetallurgy:melting/molten_desh_from_desh_dust")
+    createmetallurgy.melting(Fluid.of("createdelight:molten_ostrum", 90), "createdelight:dirty_ostrum_dust")
+        .heatRequirement("heated")
+        .id("createmetallurgy:melting/molten_ostrum_from_dirty_ostrum_dust")
+    createmetallurgy.melting(Fluid.of("createdelight:molten_ostrum", 90), "createdelight:ostrum_dust")
+        .heatRequirement("heated")
+        .id("createmetallurgy:melting/molten_ostrum_from_ostrum_dust")
+
+    createmetallurgy.melting(Fluid.of("createdelight:molten_calorite", 90), "createdelight:dirty_calorite_dust")
+        .heatRequirement("heated")
+        .id("createmetallurgy:melting/molten_calorite_from_dirty_calorite_dust")
+    createmetallurgy.melting(Fluid.of("createdelight:molten_calorite", 90), "createdelight:calorite_dust")
+        .heatRequirement("heated")
+        .id("createmetallurgy:melting/molten_calorite_from_calorite_dust")
 })
