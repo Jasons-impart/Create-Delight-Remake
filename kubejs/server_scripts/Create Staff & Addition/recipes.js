239018820799b9ccacd2af29111305fc1dca2cdc
SSWTLZZ <987508451@qq.com>
2024-10-17 23:00:14 +0800

    龙不破坏；调整cs&a的引擎配方

--- kubejs/server_scripts/Create Staff & Addition/recipes.js
+++ kubejs/server_scripts/Create Staff & Addition/recipes.js
@@ -3,13 +3,12 @@ ServerEvents.recipes(e => {
     let iner_1 = "create_sa:incomplete_hydraulic_engine"
     e.recipes.create.sequenced_assembly(
         [
-            Item.of("create_sa:hydraulic_engine").withChance(80),
-            Item.of("create:precision_mechanism").withChance(20)
-        ], "create:precision_mechanism",
+            Item.of("create_sa:hydraulic_engine")
+        ], "create:copper_sheet",
         [
             e.recipes.create.deploying(iner_1, [iner_1, "create:copper_sheet"]),
             e.recipes.create.filling(iner_1, [iner_1, Fluid.water(250)]),
-            e.recipes.create.deploying(iner_1, [iner_1, "create:copper_sheet"])
+            e.recipes.create.pressing(iner_1, iner_1)
         ]
     )
         .transitionalItem(iner_1)
@@ -26,13 +25,13 @@ ServerEvents.recipes(e => {
         .loops(3)
         .id("create_sa:heat_engine_recipe")
     let iner_3 = "create_sa:incomplete_steam_engine"
-    e.recipes.create.sequenced_assembly("create_sa:steam_engine", "create_sa:heat_engine", [
+    e.recipes.create.sequenced_assembly("create_sa:steam_engine", "create:precision_mechanism", [
         e.recipes.create.deploying(iner_3, [iner_3, "create:brass_sheet"]),
-        e.recipes.create.deploying(iner_3, [iner_3, "create_sa:hydraulic_engine"]),
+        e.recipes.create.deploying(iner_3, [iner_3, "#forge:spring/between_500_2_1000"]),
         e.recipes.create.deploying(iner_3, [iner_3, "create:propeller"])
     ])
         .transitionalItem(iner_3)
-        .loops(1)
+        .loops(3)
         .id("create_sa:steam_engine_recipe")
     e.recipes.create.mechanical_crafting(
         "create_sa:brass_jetpack_chestplate", [
