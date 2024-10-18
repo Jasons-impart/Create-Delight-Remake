931370b0dca3ccf3b165069466f092edce367259
SSWTLZZ <987508451@qq.com>
2024-10-18 09:37:52 +0800

    加入机械动力喷气背包；调冰与火设置；其他乱七八糟的

--- kubejs/server_scripts/Custom/vanilla.js
+++ kubejs/server_scripts/Custom/vanilla.js
@@ -1,6 +1,10 @@
 ServerEvents.recipes(e => {
-    const {vintageimprovements} = e.recipes
+    const {vintageimprovements, create, createmetallurgy} = e.recipes
     vintageimprovements.pressurizing("minecraft:glowstone_dust", "minecraft:glow_berries")
     .heated()
     .id("createdelight:pressurizing/glowstone_dust")
+    create.filling("minecraft:golden_apple", ["minecraft:apple", Fluid.of("createmetallurgy:molten_gold", 450)])
+    create.filling("minecraft:golden_carrot", ["minecraft:carrot", Fluid.of("createmetallurgy:molten_gold", 50)])
+    createmetallurgy.casting_in_basin("quark:golden_carrot_crate", ["farmersdelight:carrot_crate", Fluid.of("createmetallurgy:molten_gold", 450)])
+    .processingTime(100)
 })
