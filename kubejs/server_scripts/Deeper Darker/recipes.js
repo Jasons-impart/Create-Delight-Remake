9185f99b8e56c2cfecd50f9f66b2b3af5cce3f69
JasonQ1123 <nmuqindexiang@163.com>
2024-10-24 15:18:32 +0800

    大量修改

--- kubejs/server_scripts/Deeper Darker/recipes.js
+++ kubejs/server_scripts/Deeper Darker/recipes.js
@@ -1,16 +1,9 @@
 ServerEvents.recipes((e) => {
   remove_recipes_id(e, [
-    "deeperdarker:sonorous_staff",
     "silentsdelight:cutting/sculk_sensor_tendril_roll_slice",
     "silentsdelight:cutting/minced_warden_heart",
+    "silentsdelight:cutting/sculk_sensor_tendril",
   ]);
-  e.recipes.farmersdelight
-    .cutting(
-      "deeperdarker:heart_of_the_deep",
-      "#forge:tools/knives",
-      "2x silentsdelight:warden_heart"
-    )
-    .id("silentsdelight:warden_heart");
   // 增加配方：强化深板岩
   e.shaped("4x minecraft:reinforced_deepslate", ["ABA", "BCB", "ABA"], {
     A: "minecraft:ancient_debris",
@@ -22,11 +15,8 @@ ServerEvents.recipes((e) => {
     ["silentsdelight:cut_warden_ear", 2],
     ["minecraft:bone_meal"],
   ]);
-  cutting_3(e, "silentsdelight:sculk_sensor_tendril_roll", [
-    ["silentsdelight:sculk_sensor_tendril_roll_slice", 3],
-  ]);
+  cutting_3(e, "silentsdelight:sculk_sensor_tendril_roll", [["silentsdelight:sculk_sensor_tendril_roll_slice", 3]]);
   cutting_3(e, "silentsdelight:warden_heart", [["silentsdelight:minced_warden_heart", 2]]);
-  cutting_3(e, "silentsdelight:sculk_catalyst_pie", [
-    ["silentsdelight:sculk_catalyst_pie_slice", 4],
-  ]);
+  cutting_3(e, "minecraft:sculk_sensor", [["silentsdelight:sculk_sensor_tendril", 4]]);
+  cutting_3(e, "silentsdelight:sculk_catalyst_pie", [["silentsdelight:sculk_catalyst_pie_slice", 4]]);
 });
