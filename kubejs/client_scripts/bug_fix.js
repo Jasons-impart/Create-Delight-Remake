210116a3b900cb094e1882b9a09066c34fbd9c9f
SSWTLZZ <987508451@qq.com>
2024-10-26 19:48:45 +0800

    修改ae任务布局；更新todolist

--- kubejs/client_scripts/bug_fix.js
+++ kubejs/client_scripts/bug_fix.js
@@ -1,20 +1,20 @@
-BlockEvents.rightClicked("alexsmobs:capsid", (e) => {
-  const { block, player, level } = e;
-  if (
-    block.entityData["Items"][0]["id"] == "create:minecart_contraption" &&
-    player.mainHandItem.id == "create:minecart_contraption"
-  ) {
-    player.tell(Text.translate("message.createdelight.capsid"));
-    e.cancel();
-    player.sendInventoryUpdate();
-  }
-  if (
-    block.entityData["Items"][0]["id"].search("present") != -1 &&
-    player.mainHandItem.id.search("present") != -1
-  ) {
-    player.tell(Text.translate("message.createdelight.capsid"));
-    e.cancel();
-    player.sendInventoryUpdate();
-  }
-  return;
-});
+// BlockEvents.rightClicked("alexsmobs:capsid", (e) => {
+//   const { block, player, level } = e;
+//   if (
+//     block.entityData["Items"][0]["id"] == "create:minecart_contraption" &&
+//     player.mainHandItem.id == "create:minecart_contraption"
+//   ) {
+//     player.tell(Text.translate("message.createdelight.capsid"));
+//     e.cancel();
+//     player.sendInventoryUpdate();
+//   }
+//   if (
+//     block.entityData["Items"][0]["id"].search("present") != -1 &&
+//     player.mainHandItem.id.search("present") != -1
+//   ) {
+//     player.tell(Text.translate("message.createdelight.capsid"));
+//     e.cancel();
+//     player.sendInventoryUpdate();
+//   }
+//   return;
+// });
