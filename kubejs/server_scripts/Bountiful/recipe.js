c56c8eafb2bc6d79572f2e5962dfaee0d75d6e05
SSWTLZZ <987508451@qq.com>
2024-10-26 14:27:20 +0800

    小修改

---
+++ kubejs/server_scripts/Bountiful/recipe.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+    e.replaceInput({mod: "bountiful", input: "minecraft:diamond"}, "minecraft:diamond", "lightmanscurrency:trading_core")
+    e.replaceInput({mod: "bountiful", input: "minecraft:gold_ingot"}, "minecraft:gold_ingot", "lightmanscurrency:trading_core")
+})
