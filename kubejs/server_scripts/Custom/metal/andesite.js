ea148af2c9008b18170a1fb135f87e9940fcb494
SSWTLZZ <987508451@qq.com>
2024-10-28 17:34:05 +0800

    安山合金粒锭

---
+++ kubejs/server_scripts/Custom/metal/andesite.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(e => {
+    e.recipes.kubejs.shapeless("9x createdelight:andesite_alloy_nugget", "create:andesite_alloy")
+    .id("createdelight:andesite_alloy_nugget_from_andesite_alloy")
+    e.recipes.kubejs.shapeless("create:andesite_alloy", "9x createdelight:andesite_alloy_nugget")
+    .id("createdelight:andesite_alloy_nugget_from_andesite_alloy")
+})
