177bf3280ad5a381ea68131366b5e94f016ca311
SSWTLZZ <987508451@qq.com>
2024-11-10 19:57:41 +0800

    更新流体转移器的贴图，加入配方

---
+++ kubejs/server_scripts/Custom/fluid_transporter.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(e => {
+    e.recipes.kubejs.shapeless("fluid_transporter:fluid_transporter", ["supplementaries:faucet", "create_sa:large_fueling_tank"])
+    .id("fluid_transporter:fluid_transporter")
+})
