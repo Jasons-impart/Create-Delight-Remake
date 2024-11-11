81be4e5cf76e8c69a7050aa31c3ffa548bbf78d8
SSWTLZZ <987508451@qq.com>
2024-11-11 23:10:12 +0800

    击杀任务调整一下

--- kubejs/server_scripts/Improved Mobs/quest.js
+++ kubejs/server_scripts/Improved Mobs/quest.js
@@ -1,3 +1,5 @@
+//想用任务增加难度系数的，在奖励里加一个自定义，然后加上标签：rank_难度等级，比如rank_1就会增加一级
+
 /**
  * 
  * @param {Internal.Player} player 
@@ -5,7 +7,17 @@
  */
 function UpdateRank(player, value) {
     player.getServer().runCommandSilent(`/improvedmobs difficulty player ${player.username} add ${value}`)
-    player.tell(`难度提升了${value}！`)
+    if (value < 0)
+        player.tell(`难度降低了${-value}！`)
+    else
+        player.tell(`难度提升了${value}！`)
+}
+/**
+ * 
+ * @param {Internal.Player} player 
+ */
+function GetPlayerDifficulty(player) {
+    player.getData().get("improvedmobs:player_cap")
 }
 
 FTBQuestsEvents.customReward(e => {
