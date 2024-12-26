67be9e229e0c8d38a81cb36c322da3ff83cb1c5e
JasonQ1123 <nmuqindexiang@163.com>
2024-12-26 23:53:27 +0800

    111

--- kubejs/server_scripts/Custom/fluid_transporter.js
+++ kubejs/server_scripts/Custom/fluid_transporter.js
@@ -1,4 +1,4 @@
 ServerEvents.recipes(e => {
-    e.recipes.kubejs.shapeless("fluid_transporter:fluid_transporter", ["supplementaries:faucet", "create_sa:large_fueling_tank"])
-    .id("fluid_transporter:fluid_transporter")
+    // e.recipes.kubejs.shapeless("fluid_transporter:fluid_transporter", ["supplementaries:faucet", "create_sa:large_fueling_tank"])
+    // .id("fluid_transporter:fluid_transporter")
 })
