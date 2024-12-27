b0ef3e3cc37cbb12733d482f7cad606c5bff2b3e
JasonQ1123 <nmuqindexiang@163.com>
2024-12-27 12:53:59 +0800

    更新贴图，增加多流体储罐配方

--- kubejs/server_scripts/Custom/fluid_transporter.js
+++ kubejs/server_scripts/Custom/fluid_transporter.js
@@ -1,4 +1,11 @@
 ServerEvents.recipes(e => {
     // e.recipes.kubejs.shapeless("fluid_transporter:fluid_transporter", ["supplementaries:faucet", "create_sa:large_fueling_tank"])
     // .id("fluid_transporter:fluid_transporter")
+    e.recipes.create.filling(
+        'createfluidstuffs:multi_fluid_tank',
+        [
+            'create:fluid_tank',
+            Fluid.of("createmetallurgy:molten_brass", 500)
+        ]
+    ).id("createfluidstuffs:multi_fluid_tank")
 })
