895009e3a6135e6eff54d15cfad350c4fc00bc88
SSWTLZZ69 <1824914068@qq.com>
2025-01-04 18:11:51 +0800

    将jadens nether expansion与整合包适配

--- kubejs/server_scripts/Nether Exp/loot.js
+++ kubejs/server_scripts/Nether Exp/loot.js
@@ -1,4 +1,8 @@
 LootJS.modifiers(e => {
     e.addEntityLootModifier("netherexp:banshee", "netherexp:apparition", "netherexp:vessel")
-    .addLoot("art_of_forging:soul_ember").randomChance(0.5)
+        .addLoot("art_of_forging:soul_ember").randomChance(0.5)
+    e.addEntityLootModifier("minecraft:hoglin")
+        .removeLoot("netherexp:hogham")
+        .replaceLoot("farmersdelight:ham", "netherexp:hogham")
+        .replaceLoot("farmersdelight:smoked_ham", "netherexp:cooked_hogham")
 })
