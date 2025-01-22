9698e5fd7743ba2daa2cc27573761994af0a2daf
SSWTLZZ69 <1824914068@qq.com>
2025-01-22 12:53:40 +0800

    加入stonezone模组（石头变种），移除夸克箱装农作物的合成 (#145)

--- kubejs/server_scripts/Quark/recipes.js
+++ kubejs/server_scripts/Quark/recipes.js
@@ -1,4 +1,9 @@
 ServerEvents.recipes(e => {
+    remove_recipes_output(e, [
+        "quark:carrot_crate",
+        "quark:potato_crate",
+        "quark:beetroot_crate"
+    ])
     e.recipes.create.mixing(
         Fluid.of("create_enchantment_industry:experience", 10),
         'quark:ancient_fruit'
