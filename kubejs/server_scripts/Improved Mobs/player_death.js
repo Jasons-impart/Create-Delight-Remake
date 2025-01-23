c5389ace60dce7fd9cef4b5e0fb10e66e4605729
SSWTLZZ69 <1824914068@qq.com>
2025-01-23 15:16:31 +0800

    修复玩家在常规难度数值区间外死亡时的计算错误

--- kubejs/server_scripts/Improved Mobs/player_death.js
+++ kubejs/server_scripts/Improved Mobs/player_death.js
@@ -4,10 +4,10 @@ EntityEvents.death("minecraft:player", e => {
          * @type {Internal.ServerPlayer}
          */
         let player = e.entity
-        let diff = GetPlayerDifficulty(player)
         let maxrate = 0.4
         let minrate = 0.1
         let maxDiff = 250
+        let diff = min(max(GetPlayerDifficulty(player), 0), maxDiff)
         let divide = 5
         UpdateRank(player, -Math.floor(((diff / maxDiff) * minrate + (1 - diff / maxDiff) * maxrate) * diff / divide) * divide)
     }
