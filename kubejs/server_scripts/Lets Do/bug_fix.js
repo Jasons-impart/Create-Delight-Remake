dad5fd75dcde7bab08671be17a513eb1b6d63e61
SSWTLZZ69 <1824914068@qq.com>
2025-01-24 13:15:19 +0800

    修复vinery流体附属的注液和分液配方丢失

--- kubejs/server_scripts/Lets Do/bug_fix.js
+++ kubejs/server_scripts/Lets Do/bug_fix.js
@@ -15,3 +15,25 @@ ServerEvents.tags("item", e => {
         "vinery:cherry",
     ])
 })
+ServerEvents.recipes(e => {
+    e.remove({type: "create:filling", input: "vinery:wine_bottle"})
+    e.remove({type: "create:emptying", output: "vinery:wine_bottle"})
+    let grape_list = [
+        ["vinery:red_grapejuice", "doaddonfluids:red_grapejuice"],
+        ["vinery:red_jungle_grapejuice", "doaddonfluids:jungle_red_grapejuice"],
+        ["vinery:red_savanna_grapejuice", "doaddonfluids:savanna_red_grapejuice"],
+        ["vinery:red_taiga_grapejuice", "doaddonfluids:taiga_red_grapejuice"],
+        ["vinery:white_grapejuice", "doaddonfluids:white_grapejuice"],
+        ["vinery:white_jungle_grapejuice", "doaddonfluids:jungle_white_grapejuice"],
+        ["vinery:white_savanna_grapejuice", "doaddonfluids:savanna_white_grapejuice"],
+        ["vinery:white_taiga_grapejuice", "doaddonfluids:taiga_white_grapejuice"],
+        ["nethervinery:warped_grapejuice", "doaddonfluids:warped_grapejuice"],
+        ["nethervinery:crimson_grapejuice", "doaddonfluids:crimson_grapejuice"]
+    ]
+    grape_list.forEach(grape => {
+        e.recipes.create.filling(grape[0], ["vinery:wine_bottle", Fluid.of(grape[1], 250)])
+        .id(`doaddonfluids:filling/${grape[1].split(":")[1]}`)
+        e.recipes.create.emptying(["vinery:wine_bottle", Fluid.of(grape[1], 250)], grape[0])
+        .id(`doaddonfluids:emptying/${grape[0].split(":")[1]}`)
+    })
+})
