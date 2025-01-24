1012d6248a600394fbc14286d305efd53c580bcb
SSWTLZZ69 <1824914068@qq.com>
2025-01-24 13:15:48 +0800

    修复vinery流体附属的注液和分液配方丢失 (#155)

--- kubejs/server_scripts/Lets Do/remove_fxxking_things.js
+++ kubejs/server_scripts/Lets Do/remove_fxxking_things.js
@@ -5,6 +5,4 @@ ServerEvents.recipes(e => {
     remove_recipes_output(e, [
         "vinery:cherry_boat"
     ])
-    e.remove({mod: "vinery", type: "create:mixing"})
-    e.remove({mod: "vinery", type: "create:compacting"})
 })
