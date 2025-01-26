12c1cfd0245bd96911b16f75b7e50edf573327db
AzureCrab <wanquan.wu.ca@gmail.com>
2025-01-26 16:12:17 +0800

    [feat][ftb]gateway任务书初提交-by爱玩不爱玩_ 重写cawyyds任务，增加电动力学章节，修改部分不合适的任务连线； 汉化extendAE； 替换neapolitan的红豆植株的战利品表以防止掉落无用红豆(#161)
    * [feat][ftb]gateway任务书初提交-by爱玩不爱玩_
    
    * 移除相关链接
    
    * 调整章节位置
    
    * 重写cawyyds任务，增加电动力学章节，修改部分不合适的任务连线；
    汉化extendAE；
    替换neapolitan的红豆植株的战利品表以防止掉落无用红豆
    
    ---------
    
    Co-authored-by: JasonQ1123 <nmuqindexiang@163.com>

--- kubejs/server_scripts/Neapolitan/loot.js
+++ kubejs/server_scripts/Neapolitan/loot.js
@@ -1,4 +1,6 @@
 LootJS.modifiers(e => {
     e.addBlockLootModifier("neapolitan:adzuki_soil")
     .replaceLoot("neapolitan:adzuki_beans", "createdelight:adzuki_beans_seed")
+    e.addBlockLootModifier("neapolitan:adzuki_sprouts")
+    .replaceLoot("neapolitan:adzuki_beans", "createdelight:adzuki_beans_seed")
 })
