9da792acf966983fc81922f1d4ce8f1f81fc561d
Azure Crab <wuwanquan@126.com>
2025-01-31 01:09:10 +0800

    [refactor]adastra的锤子已移除，移除相关的kubejs改动

--- kubejs/server_scripts/Adastra/loot.js
+++ kubejs/server_scripts/Adastra/loot.js
@@ -1,7 +1,4 @@
 LootJS.modifiers(e => {
-    // 移除adastra中锤子的生成
-    e.addLootTableModifier("ad_astra:chests/village/moon/blacksmith")
-        .removeLoot("ad_astra:hammer")
     e.addEntityLootModifier("ad_astra:corrupted_lunarian")
         .addLoot('minecraft:glow_ink_sac')
 })
