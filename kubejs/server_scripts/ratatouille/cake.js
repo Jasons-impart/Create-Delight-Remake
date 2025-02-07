0a5905202f8f7f2a5f5e4291fba87f1c28792d82
SSWTLZZ69 <1824914068@qq.com>
2025-02-07 19:47:05 +0800

    加入abnormal delight（为那不勒斯的蛋糕增加了蛋糕切片）

--- kubejs/server_scripts/ratatouille/cake.js
+++ kubejs/server_scripts/ratatouille/cake.js
@@ -48,7 +48,6 @@ ServerEvents.recipes(e => {
             Fluid.of("create:chocolate", 1000)
         ]
     ).id("create:filling/chocolate_cake")
-    cutting(e, 'neapolitan:chocolate_cake', [['create_central_kitchen:chocolate_cake_slice', 7]])
     e.recipes.create.sequenced_assembly("farmersrespite:coffee_cake", "ratatouille:cake_base", [
         e.recipes.create.filling("ratatouille:cake_base", ["ratatouille:cake_base", Fluid.of("farmersrespite:coffee", 1000)]),
         e.recipes.create.deploying("ratatouille:cake_base", ["ratatouille:cake_base", "createcafe:roasted_coffee_beans"])
