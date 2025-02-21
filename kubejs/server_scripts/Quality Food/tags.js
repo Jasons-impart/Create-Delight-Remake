4dd49c1464e374f023afa1ff6bdacd2a27cdb397
SSWTLZZ69 <1824914068@qq.com>
2025-02-21 16:17:49 +0800

    加入quality food模组（为作物和食物添加品质）

---
+++ kubejs/server_scripts/Quality Food/tags.js
@@ -0,0 +1,6 @@
+ServerEvents.tags("minecraft:block", e => {
+    e.add("quality_food:quality_blocks",
+        "vintagedelight:peanut_crate",
+        "vintagedelight:gearo_berry_bag",
+        "neapolitan:adzuki_crate")
+})
