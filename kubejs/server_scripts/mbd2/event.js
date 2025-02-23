feb11d393e3be7f3ccf611c0ac8cc1441e29f330
SSWTLZZ69 <1824914068@qq.com>
2025-02-23 22:46:19 +0800

    一个测试机器 加入powerfulJS以可以在kubejs中使用Capabilities (#259)

--- kubejs/server_scripts/mbd2/event.js
+++ kubejs/server_scripts/mbd2/event.js
@@ -1,47 +1,47 @@
-
-    /**
-     * 
-     * @param {Internal.Level} level 
-     * @param {BlockPos} pos 
-     * @param {number} age 
-     * @param {Map<string, number>} list 
-     * @returns {Map<string, number>}
-     */
-    function nuclearDiffusionByAge(level, pos, age, list) {
-        if (age == 0)
-            return -1
-        let positions = [pos.above(), pos.below(), pos.north(), pos.south(), pos.east(), pos.west()]
-        if (!level.getBlock(pos).hasTag("create_new_age:stops_radiation")) {
-            list.set(`${pos.x},${pos.y},${pos.z}`, age)
-            positions.forEach(position => {
-                if (list.get(`${position.x},${position.y},${position.z}`) == null) {
-                    nuclearDiffusionByAge(level, position, age - 1, list)
-                }
-            })
-        }
+const $ContentModifier = Java.loadClass("com.lowdragmc.mbd2.api.recipe.content.ContentModifier")
+/**
+ * 
+ * @param {Internal.Level} level 
+ * @param {BlockPos} pos 
+ * @param {number} age 
+ * @param {Map<string, number>} list 
+ * @returns {Map<string, number>}
+ */
+function nuclearDiffusionByAge(level, pos, age, list) {
+    if (age == 0)
+        return -1
+    let positions = [pos.above(), pos.below(), pos.north(), pos.south(), pos.east(), pos.west()]
+    if (!level.getBlock(pos).hasTag("create_new_age:stops_radiation")) {
+        list.set(`${pos.x},${pos.y},${pos.z}`, age)
+        positions.forEach(position => {
+            if (list.get(`${position.x},${position.y},${position.z}`) == null) {
+                nuclearDiffusionByAge(level, position, age - 1, list)
+            }
+        })
     }
+}
 
-    /**
-     * 
-     * @param {Internal.Level} level
-     * @param {BlockPos} pos
-     * @param {number} size
-     * @param {Map<string, number>} list
-     * @returns {Map<string, number>}
-     */
-    function nuclearDiffusionByCount(level, pos, size, list) {
-        if (list.size >= size)
-            return
-        let positions = [pos.above(), pos.below(), pos.north(), pos.south(), pos.east(), pos.west()]
-        if (!level.getBlock(pos).hasTag("create_new_age:stops_radiation")) {
-            list.set(`${pos.x},${pos.y},${pos.z}`, age)
-            positions.forEach(position => {
-                if (list.get(`${position.x},${position.y},${position.z}`) == null) {
-                    nuclearDiffusionByAge(level, position, age - 1, list)
-                }
-            })
-        }
+/**
+ * 
+ * @param {Internal.Level} level
+ * @param {BlockPos} pos
+ * @param {number} size
+ * @param {Map<string, number>} list
+ * @returns {Map<string, number>}
+ */
+function nuclearDiffusionByCount(level, pos, size, list) {
+    if (list.size >= size)
+        return
+    let positions = [pos.above(), pos.below(), pos.north(), pos.south(), pos.east(), pos.west()]
+    if (!level.getBlock(pos).hasTag("create_new_age:stops_radiation")) {
+        list.set(`${pos.x},${pos.y},${pos.z}`, age)
+        positions.forEach(position => {
+            if (list.get(`${position.x},${position.y},${position.z}`) == null) {
+                nuclearDiffusionByAge(level, position, age - 1, list)
+            }
+        })
     }
+}
 MBDMachineEvents.onTick("mbd2:reactor_rod", e => {
     const { level, pos, machineStateName } = e.event.machine
     if (machineStateName == "working") {
@@ -75,7 +75,6 @@ MBDMachineEvents.onTick("mbd2:reactor_rod", e => {
 })
 
 MBDMachineEvents.onRemoved("mbd2:reactor_rod", e => {
-    
     const { level, pos, machineStateName } = e.event.machine
     if (machineStateName == "working" || machineStateName == "waiting") {
         level.createExplosion(pos.x, pos.y, pos.z)
@@ -83,4 +82,25 @@ MBDMachineEvents.onRemoved("mbd2:reactor_rod", e => {
         .explosionMode("block")
         .explode()
     }
+})
+
+MBDMachineEvents.onBeforeRecipeModify("mbd2:centrifuge", e => {
+    /**
+     * @type {Internal.KineticBlockEntity}
+     */
+    let be = e.event.machine.machineHolder
+    let inventory = e.event.machine.getCapability(ForgeCapabilities.ITEM_HANDLER).orElse(null)
+    let item = inventory.getStackInSlot(0)
+    function linear(val, start, end) {
+        return val * (end - start) + start
+    }
+    let durationMultipler = li446near(be.speed / 256, 4, 1)
+    let count = item.getCount()
+    if (count >= 16 && count <= item.getMaxStackSize()) {
+        durationMultipler *= linear((count - 16)/(item.getMaxStackSize() - 16), 1, 2)
+    }
+    let oldRecipe = e.event.recipe.copy()
+    oldRecipe.duration *= durationMultipler
+    e.event.setRecipe(oldRecipe)
+    console.log(`recipe_modified. itemcount:${count}, durationMultipler:${1 / durationMultipler}`)
 })
