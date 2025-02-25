b4e7c55d0fe399ad8ce6891fb619a63383d3256f
SSWTLZZ69 <1824914068@qq.com>
2025-02-25 18:24:25 +0800

    增加读取某json给称号的功能；加入fluidlogged(使所有含水方块可以含任何流体)；修正一下锻造匠的一个交易 (#268)

---
+++ kubejs/server_scripts/Custom/player_login.js
@@ -0,0 +1,15 @@
+
+PlayerEvents.loggedIn(e => {
+    const {level, player} = e
+    let donate_list = JsonIO.read("donate_list.json")
+    if (player.persistentData.get("notFirstLogin") == null) {
+        player.persistentData.putBoolean("notFirstLogin", true)
+        let name = player.name.getString()
+        let title = donate_list.get(name)
+        if (title != null) {
+            level.runCommandSilent(`/ftbranks create ${name}`)
+            level.runCommandSilent(`/ftbranks add ${name} ${name.toLowerCase()}`)
+            level.runCommandSilent(`/ftbranks node add ${name.toLowerCase()} ftbranks.name_format [${title}]{name}`)
+        }
+    }
+})
