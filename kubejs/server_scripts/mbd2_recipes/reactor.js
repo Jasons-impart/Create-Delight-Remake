3ceda0c3d4cface6121cdd7c12082a474ec9650b
SSWTLZZ69 <1824914068@qq.com>
2025-02-28 12:06:16 +0800

    调整cc&a和newage的su电转化效率（除5）,调整mbd机器的产电和耗电量

--- kubejs/server_scripts/mbd2_recipes/reactor.js
+++ kubejs/server_scripts/mbd2_recipes/reactor.js
@@ -3,14 +3,14 @@ ServerEvents.recipes(e => {
     e.recipes.mbd2.reactor()
      .inputItems("alexscaves:uranium_rod")
      .perTick(builder => builder
-        .outputFE(2500)
+        .outputFE(1500)
      )
      .duration(600)
     //核反应燃料发电
      e.recipes.mbd2.reactor()
      .inputItems("create_new_age:nuclear_fuel")
      .perTick(builder => builder
-        .outputFE(1000)
+        .outputFE(750)
      )
      .duration(600)
 })
