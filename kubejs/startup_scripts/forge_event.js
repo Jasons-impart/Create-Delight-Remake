34d067cd02e50d90754e719d6deea3ca9ee256f2
SSWTLZZ69 <1824914068@qq.com>
2025-03-10 21:30:30 +0800

    加入威压tetra效果；将龙钢（7级锤）和悚怖（8级锤）加入创造模式物品栏 (#307)

--- kubejs/startup_scripts/forge_event.js
+++ kubejs/startup_scripts/forge_event.js
@@ -2,6 +2,7 @@ ForgeEvents.onEvent("net.minecraftforge.event.entity.living.LivingHurtEvent", e 
     try{
         global.ArmorplateHurtEvent(e)
         global.TetraForstallAttackEffect(e)
+        global.TetraOverwhelmAttackEffect(e)
     }catch(err){
         console.error(err)
     }
