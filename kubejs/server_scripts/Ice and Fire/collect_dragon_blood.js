ea48eea605d31f237889a63b41e7b8aff0268847
SSWTLZZ69 <1824914068@qq.com>
2025-03-14 20:41:11 +0800

    修复一下逻辑

--- kubejs/server_scripts/Ice and Fire/collect_dragon_blood.js
+++ kubejs/server_scripts/Ice and Fire/collect_dragon_blood.js
@@ -1,9 +1,8 @@
 const $EntityDragonBase = Java.loadClass("com.github.alexthe666.iceandfire.entity.EntityDragonBase")
 
 ItemEvents.entityInteracted("createdelight:blood_collection_device", e => {
-    if (e.player.cooldowns.getCooldowns().get("createdelight:blood_collection_device") > 0)
+    if (e.player.cooldowns.isOnCooldown("createdelight:blood_collection_device"))
         return
-    console.log("开始了")
     let entity = e.target
     if (!entity || !entity.living)
         return
@@ -11,15 +10,12 @@ ItemEvents.entityInteracted("createdelight:blood_collection_device", e => {
     if (!e.player.mainHandItem == "createdelight:blood_collection_device" || !e.player.offHandItem == "minecraft:glass_bottle")
         return
 
-    console.log("检查完了")
     if (entity instanceof $EntityDragonBase) {
         /**
          * @type {Internal.EntityDragonBase}
          */
         let dragon = entity
-        console.log("到龙这里了")
         if (dragon.isTame() && dragon.isAlive()) {
-            console.log("判断是驯服的龙了")
             let maxhp = dragon.maxHealth
             let damage = Math.max(50, maxhp * 0.1)
             e.server.runCommandSilent(`damage ${dragon.uuid} ${damage}`)
@@ -34,6 +30,7 @@ ItemEvents.entityInteracted("createdelight:blood_collection_device", e => {
                 e.player.give("iceandfire:lightning_dragon_blood")
             }
             e.player.cooldowns.addCooldown(e.player.mainHandItem.item, 60)
+            e.player.mainHandItem.shrink(1)
             e.player.offHandItem.shrink(1)
         }
     }
