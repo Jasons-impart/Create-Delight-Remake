0d5af7f7dfdc0bf0f8636de3c178e1642d9c247a
SSWTLZZ69 <1824914068@qq.com>
2025-03-14 21:13:05 +0800

    一个采血器（可以采集龙的血 (#319)
    * 一个采血器（可以采集龙的血
    
    * 修复一下逻辑

---
+++ kubejs/server_scripts/Ice and Fire/collect_dragon_blood.js
@@ -0,0 +1,37 @@
+const $EntityDragonBase = Java.loadClass("com.github.alexthe666.iceandfire.entity.EntityDragonBase")
+
+ItemEvents.entityInteracted("createdelight:blood_collection_device", e => {
+    if (e.player.cooldowns.isOnCooldown("createdelight:blood_collection_device"))
+        return
+    let entity = e.target
+    if (!entity || !entity.living)
+        return
+    //限制主手使用
+    if (!e.player.mainHandItem == "createdelight:blood_collection_device" || !e.player.offHandItem == "minecraft:glass_bottle")
+        return
+
+    if (entity instanceof $EntityDragonBase) {
+        /**
+         * @type {Internal.EntityDragonBase}
+         */
+        let dragon = entity
+        if (dragon.isTame() && dragon.isAlive()) {
+            let maxhp = dragon.maxHealth
+            let damage = Math.max(50, maxhp * 0.1)
+            e.server.runCommandSilent(`damage ${dragon.uuid} ${damage}`)
+            e.player.doHurtTarget(entity)
+            if (dragon.type == "iceandfire:fire_dragon") {
+                e.player.give("iceandfire:fire_dragon_blood")
+            }
+            else if (dragon.type == "iceandfire:ice_dragon") {
+                e.player.give("iceandfire:ice_dragon_blood")
+            }
+            else if (dragon.type == "iceandfire:lightning_dragon") {
+                e.player.give("iceandfire:lightning_dragon_blood")
+            }
+            e.player.cooldowns.addCooldown(e.player.mainHandItem.item, 60)
+            e.player.mainHandItem.shrink(1)
+            e.player.offHandItem.shrink(1)
+        }
+    }
+})
