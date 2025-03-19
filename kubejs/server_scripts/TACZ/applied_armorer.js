bff3f27bd8d29cf7bbc3021a699f4a59d87d3bd6
SSWTLZZ69 <1824914068@qq.com>
2025-03-19 11:36:21 +0800

    军械师的一些配件的配方

--- kubejs/server_scripts/TACZ/applied_armorer.js
+++ kubejs/server_scripts/TACZ/applied_armorer.js
@@ -39,4 +39,128 @@ ServerEvents.recipes(e => {
         Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:bracelet_niklas"}').strongNBT(),
         "#forge:ingots/electrum"
     ).id("applied_armorer:bracelet_aerial_wristband")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:si_double_sided_mirror"}'), [
+        "A A",
+        "BCB"
+    ], {
+        A: "ae2:quartz_glass",
+        B: "megacells:sky_steel_ingot",
+        C: "ae2:fluix_crystal"
+    })
+    .id("applied_armorer:si_double_sided_mirror")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:si_profession"}'), [
+        "A  ",
+        "BBB"
+    ], {
+        A: "ae2:quartz_glass",
+        B: "megacells:sky_steel_ingot"
+    })
+    .id("applied_armorer:si_profession")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:scope_xgs_905"}'), [
+        "AAA",
+        "B B",
+        "AAA"
+    ], {
+        A: "megacells:sky_steel_ingot",
+        B: "ae2:quartz_glass"
+    })
+    .id("applied_armorer:scope_xgs_905")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:si_simple_3"}'), [
+        " A ",
+        " B ",
+        " A "
+    ], {
+        A: "megacells:sky_steel_ingot",
+        B: "ae2:quartz_glass"
+    })
+    .id("applied_armorer:si_simple_3")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:sight_type_3741"}'), [
+        "ABA",
+    ], {
+        A: "megacells:sky_steel_ingot",
+        B: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:si_simple_3"}').strongNBT()
+    })
+    .id("applied_armorer:sight_type_3741")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:si_ms_12"}'), [
+        " A ",
+        "BCB",
+        "AAA"
+    ], {
+        A: "megacells:sky_steel_ingot",
+        B: "ae2:quartz_glass",
+        C: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:sight_type_3741"}').strongNBT()
+    })
+    .id("applied_armorer:scope_ms_12")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:scope_ms_14"}'), [
+        "AAA",
+        "BCB",
+        "AAA"
+    ], {
+        A: "megacells:sky_steel_ingot",
+        B: "ae2:quartz_glass",
+        C: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:si_ms_12"}').strongNBT()
+    })
+    .id("applied_armorer:scope_ms_14")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_ns_1"}'), [
+        "AAA",
+        "B B",
+        "AAA"
+    ], {
+        A: "#forge:rods/iron",
+        B: "#forge:nuggets/iron"
+    })
+    .id("applied_armorer:muzzle_ns_1")
+    custom_inscribe(
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_firefly"}'),
+        "ae2:calculation_processor",
+        "#forge:plates/iron",
+        "ae2:logic_processor"
+    ).id("applied_armorer:muzzle_chip_firefly")
+    custom_inscribe(
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_pcs_x1"}'),
+        "ae2:speed_card",
+        "createdelight:forged_steel_sheet",
+        "ae2:calculation_processor"
+    ).id("applied_armorer:muzzle_chip_pcs_x1")
+    custom_inscribe(
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_atm_x2"}'),
+        "ae2:speed_card",
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_pcs_x1"}').strongNBT(),
+        "ae2:engineering_processor"
+    ).id("applied_armorer:muzzle_chip_pcs_x1")
+    custom_inscribe(
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_firework"}'),
+        "ae2:energy_card",
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_firefly"}').strongNBT(),
+        "ae2:energy_cell"
+    ).id("applied_armorer:muzzle_chip_firework")
+    custom_inscribe(
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_hyper_propellant"}'),
+        "ae2:energy_card",
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_firework"}').strongNBT(),
+        "ae2:dense_energy_cell"
+    ).id("applied_armorer:muzzle_chip_hyper_propellant")
+    custom_inscribe(
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_fat_boy"}'),
+        "megacells:greater_energy_card",
+        Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_hyper_propellant"}').strongNBT(),
+        "megacells:mega_energy_cell"
+    ).id("applied_armorer:muzzle_chip_fat_boy")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_commander"}'), [
+        "AAA",
+        "   ",
+        "BAA"
+    ], {
+        A: "#forge:rods/iron",
+        B: "#forge:nuggets/iron"
+    }).id("applied_armorer:muzzle_commander")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_bs_mod4"}'), [
+        "ABA",
+        "   ",
+        "ABA"
+    ], {
+        A: "#forge:rods/iron",
+        B: "#forge:nuggets/iron"
+    }).id("applied_armorer:muzzle_bs_mod4")
+    
 })
