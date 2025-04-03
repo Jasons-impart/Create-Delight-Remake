035c9126234cad33c33ffb588c6687573392e73e
JasonQ <154645632+JasonQ1123@users.noreply.github.com>
2025-04-03 20:07:17 +0800

    修复多个黄瓜问题，修复农民交易问题，增加农民交易，增加可 compost 物品 (#410)

--- kubejs/server_scripts/util/trade.js
+++ kubejs/server_scripts/util/trade.js
@@ -7,11 +7,23 @@ let TradeUtil = {}
  * @param {number} index 
  */
 TradeUtil.replaceVillageTradeIndex = function(e, villager, trades, index) {
-    if (villager.startsWith("minecraft:") || !villager.includes(":"))
-        e.removeVanillaTrades([villager], index)
-    else
-        e.removeModdedTrades([villager], index)
+    // if (villager.startsWith("minecraft:") || !villager.includes(":"))
+    //     e.removeVanillaTrades([villager], index)
+    // else
+    e.removeModdedTrades([villager], index)
     trades.forEach(trade => {
         e.addTrade(villager, index, trade[0], trade[1])
     })
+}
+/**
+ * 
+ * @param {Internal.VillagerTradingEventJS} e 
+ * @param {Internal.VillagerProfession_} villager 
+ * @param {[InputItem_[], InputItem_][]} trades
+ * @param {number} index 
+ */
+TradeUtil.addVillageTradeIndex = function(e, villager, trades, index) {
+   trades.forEach(trade => {
+       e.addTrade(villager, index, trade[0], trade[1])
+   }) 
 }
