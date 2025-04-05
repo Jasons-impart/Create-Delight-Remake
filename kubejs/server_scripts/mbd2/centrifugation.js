facba4804a4b847fd19bddedc3138ea4b176abc4
SSWTLZZ69 <1824914068@qq.com>
2025-04-05 16:02:57 +0800

    修复一下tetra小刀的一个报错；增加一个转速影响速度的事件 (#424)

---
+++ kubejs/server_scripts/mbd2/centrifugation.js
@@ -0,0 +1,27 @@
+MBDMachineEvents.onBeforeRecipeModify("createdelight:big_centrifuge", e => {
+    /**
+     * @type {Internal.MBDMultiblockMachine}
+     */
+    let multiblock = e.event.machine
+    let maxSpeed = -1
+    multiblock.parts.forEach(part => {
+        /**
+         * @type {Internal.MBDPartMachine}
+         */
+        let partMachine = part
+        if (partMachine.definition.id() == "createdelight:create_in") {
+            /**
+             * @type {Internal.KineticBlockEntity}
+             */
+            let kineticInput = partMachine.machineHolder
+            maxSpeed = Math.max(kineticInput.speed, speed)
+        }
+    })
+    function linear(val, start, end) {
+        return val * (end - start) + start
+    }
+    let durationMultipler = linear((maxSpeed - 32) / (256 - 32), 4, 1)
+    let oldRecipe = e.event.recipe.copy()
+    oldRecipe.duration *= durationMultipler
+    e.event.setRecipe(oldRecipe)
+})
