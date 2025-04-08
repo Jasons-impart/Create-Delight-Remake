bf4cf457d0fb721d3d16947eb85376f117638a35
SSWTLZZ69 <1824914068@qq.com>
2025-04-08 15:48:13 +0800

    加入quality food，使得只会在沃土与复生沃土（及其耕地）上产出带有品质的作物，并且适配一些mod的箱装和袋装食物 (#434)
    * 加入quality food，使得只会在沃土与复生沃土（及其耕地）上产出带有品质的作物，并且适配一些mod的箱装和袋装食物
    
    * 调整设置

---
+++ kubejs/server_scripts/Quality Food/tags.js
@@ -0,0 +1,16 @@
+ServerEvents.tags("block", e => {
+    Ingredient.of([
+        "@culturaldelights",
+        "@fruitsdelight",
+        "@collectorsreap",
+        "@neapolitan",
+        "@mynethersdelight",
+        "@oceanic_delight",
+        "@frycooks_delight",
+        "@corn_delight"
+        ]).stacks.forEach(item => {
+        if ((item.id.endsWith("_crate") || item.id.endsWith("_bag")) && item.block) {
+            e.add("quality_food:quality_blocks", item.id)
+        }
+    })
+})
