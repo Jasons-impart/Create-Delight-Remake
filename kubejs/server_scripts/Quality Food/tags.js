f307ff98441c1a65ff065391f4f52c0165e7c9bf
SSWTLZZ69 <1824914068@qq.com>
2025-04-13 19:34:53 +0800

    修改一个作物产生另一个某品质作物的计算公式；补全作物的tag

--- kubejs/server_scripts/Quality Food/tags.js
+++ kubejs/server_scripts/Quality Food/tags.js
@@ -9,9 +9,20 @@ ServerEvents.tags("block", e => {
         "@oceanic_delight",
         "@frycooks_delight",
         "@corn_delight"
-        ]).stacks.forEach(item => {
+    ]).stacks.forEach(item => {
         if ((item.id.endsWith("_crate") || item.id.endsWith("_bag")) && item.block) {
             e.add("quality_food:quality_blocks", item.id)
         }
     })
+    e.add("createdelight:quality_crops",
+        "#minecraft:crops",
+        "farmersrespite:tea_bush",
+        "farmersrespite:small_tea_bush",
+        "neapolitan:vanilla_vine",
+        "neapolitan:strawberry_bush",
+        "neapolitan:small_banana_frond",
+        "neapolitan:banana_frond",
+        "neapolitan:large_banana_frond",
+        "neapolitan:mint"
+    )
 })
