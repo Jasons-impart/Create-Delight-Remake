6398bf4925029cc1b6e14e76d97ca7b607f71a9d
AzureCrab <wanquan.wu.ca@gmail.com>
2025-04-20 00:38:10 +0800

    禁止quark纸灯笼放饰品栏腰带，避免导致崩溃; AE2终端风格改默认最长；开发版标题界面显示版本号为Dev (#476)
    * AE2终端风格改默认最长；开发版标题界面显示版本号为Dev
    
    * 禁止quark纸灯笼放饰品栏腰带，避免导致崩溃

--- config/fancymenu/assets/version.md
+++ config/fancymenu/assets/version.md
@@ -1,4 +1,4 @@
 |||
-Create-Delight-Remake 0.3.2.3 test-only-03271435
+Create-Delight-Remake Dev
 MIT License Copyright (c) 2025 JSI Production Team
 |||
---
+++ kubejs/server_scripts/Quark/tags.js
@@ -0,0 +1,6 @@
+ServerEvents.tags("item", e => {
+    e.remove("curios:belt", [
+        "quark:paper_lantern",
+        "quark:paper_lantern_sakura"
+    ])
+})
