eb5cfd39f374d00654a3b2f244e11ea407364756
SSWTLZZ69 <1824914068@qq.com>
2025-04-19 09:59:52 +0800

    解决服务端的问题

--- kubejs/client_scripts/network.js
+++ kubejs/client_scripts/network.js
@@ -1,15 +1,12 @@
 
+const $FluidFX = Java.loadClass("com.simibubi.create.content.fluids.FluidFX")
 NetworkEvents.dataReceived("kubejs_player_playsound", e => {
     e.player.playSound(e.data.get("soundEvent").getAsString())
 })
 
 NetworkEvents.dataReceived("spawn_create_particle", e => {
-    const {particle, x, y, z, vx, vy, vz} = e.data
-    /**
-     * @type {Internal.FluidParticleData}
-     */
-    let p = Utils.particleOptions(particle)
+    const {fluid, x, y, z, vx, vy, vz} = e.data
+    let particle = $FluidFX.getFluidParticle(Fluid.of(fluid).fluid)
     
-    e.player.level.addParticle(p, x, y, z, vx, vy, vz)
-    // console.log(particle)
+    e.player.level.addParticle(particle, x, y, z, vx, vy, vz)
 })
