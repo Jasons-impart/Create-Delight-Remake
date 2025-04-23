761904675be54367eb3cfde0357cd099a2aff564
SSWTLZZ69 <1824914068@qq.com>
2025-04-23 10:31:50 +0800

    修改洒水器的逻辑（改为范围为以向下射线碰到的第一个方块为球心半径为5的球体）；使出货箱可以出售零散物品 (#483)

--- kubejs/server_scripts/mbd2/sprinkler.js
+++ kubejs/server_scripts/mbd2/sprinkler.js
@@ -2,26 +2,44 @@
 MBDMachineEvents.onRecipeWorking("createdelight:sprinkler", e => {
     let event = e.event
     const { machine } = event
+    const {customData, level} = machine
+    if (customData.getIntArray("solidBlockPos") != null) {
+        let array = customData.getIntArray("solidBlockPos")
+        customData.putIntArray("oldSolidBlockPos", [array[0], array[1], array[2]])
+    }
+
+    let tmpSolidBlockPos = machine.pos
+    while (level.getBlock(tmpSolidBlockPos.below()).getBlockState().isAir())
+        tmpSolidBlockPos = tmpSolidBlockPos.below()
+    customData.putIntArray("solidBlockPos", [tmpSolidBlockPos.x, tmpSolidBlockPos.y, tmpSolidBlockPos.z])
     let hl = 1
     let rl = 25
-    let manager = SeasonUtil.getSaveData(machine.level)
-    let nearHumidityControlProvider = manager.queryHumidityControlProvider(machine.pos)
+    let rawOldSolidBlockPos = customData.getIntArray("oldSolidBlockPos")
+    let rawSolidBlockPos = customData.getIntArray("solidBlockPos")
+    let solidBlockPos = [rawSolidBlockPos[0], rawSolidBlockPos[1], rawSolidBlockPos[2]]
+    let oldSolidBlockPos = [rawOldSolidBlockPos[0], rawOldSolidBlockPos[1], rawOldSolidBlockPos[2]]
+    let manager = SeasonUtil.getSaveData(level)
+    if (oldSolidBlockPos != null && (oldSolidBlockPos[0] != solidBlockPos[0] || oldSolidBlockPos[1] != solidBlockPos[1] || oldSolidBlockPos[2] != solidBlockPos[2])) {
+            manager.removeHumidityControlProvider(oldSolidBlockPos)
+    }
+    let nearHumidityControlProvider = manager.queryHumidityControlProvider(solidBlockPos)
     if (nearHumidityControlProvider != null
         && hl == nearHumidityControlProvider.getLevel()
+
         && rl == nearHumidityControlProvider.getRange()
     ) {
         if (nearHumidityControlProvider.getRemainTime() < 100000) {
-            nearHumidityControlProvider.addRemainTime(100);
+            nearHumidityControlProvider.addRemainTime(100)
         }
     } else {
         if (hl != 0)
-            manager.addMap(machine.pos, SeasonUtil.createHumidityControlProvider(hl, rl, 500));
+            manager.addMap(solidBlockPos, SeasonUtil.createHumidityControlProvider(hl, rl, 500))
     }
     let fluid = machine.getCapability(ForgeCapabilities.FLUID_HANDLER).orElse(null)
     if (fluid) {
         // console.log("hahaha")
         let x = 0, z = 0;
-        let random = machine.level.random
+        let random = level.random
         let vx = 0, vz = 0
         let y = random.nextDouble() - 1
         if (random.nextBoolean()) {
@@ -36,8 +54,7 @@ MBDMachineEvents.onRecipeWorking("createdelight:sprinkler", e => {
             .add([0.0, y * 0.3 - 0.1, 0.0])
             .add([x * 0.3, 0.0, z * 0.3])
         try {
-            let level = machine.level
-            // console.log(machine.level)
+            // console.log(level)
 
             level.getPlayers().forEach(player => {
                 /**@type {Internal.Player} */(player).sendData("spawn_create_particle", {
@@ -55,7 +72,7 @@ MBDMachineEvents.onRecipeWorking("createdelight:sprinkler", e => {
             // level.spawnParticles(particle, false,
             //         vec.x(), vec.y(), vec.z(),
             //         x * 0.2, -0.1, z * 0.2, 1, 0.5)
-            // machine.level.addParticle(particle,
+            // level.addParticle(particle,
             //     vec.x(), vec.y(), vec.z(),
             //     x * 0.2, -0.1, z * 0.2)
         } catch (error) {
@@ -66,11 +83,17 @@ MBDMachineEvents.onRecipeWorking("createdelight:sprinkler", e => {
 })
 MBDMachineEvents.onTick("createdelight:sprinkler", e => {
     let event = e.event
-    const { machine, } = event
+    const { machine} = event
+    const {customData, level} = machine
+    let rawOldSolidBlockPos = customData.getIntArray("oldSolidBlockPos")
+    let rawSolidBlockPos = customData.getIntArray("solidBlockPos")
+    let solidBlockPos = [rawSolidBlockPos[0], rawSolidBlockPos[1], rawSolidBlockPos[2]]
+    let oldSolidBlockPos = [rawOldSolidBlockPos[0], rawOldSolidBlockPos[1], rawOldSolidBlockPos[2]]
     if (machine.machineStateName == "base") {
-        let manager = SeasonUtil.getSaveData(machine.level)
+        let manager = SeasonUtil.getSaveData(level)
         if (manager != null) {
-            manager.removeHumidityControlProvider(machine.pos)
+            manager.removeHumidityControlProvider(oldSolidBlockPos)
+            manager.removeHumidityControlProvider(solidBlockPos)
         }
     }
 })
@@ -78,8 +101,14 @@ MBDMachineEvents.onTick("createdelight:sprinkler", e => {
 MBDMachineEvents.onRemoved("createdelight:sprinkler", e => {
     let event = e.event
     const { machine } = event
-    let manager = SeasonUtil.getSaveData(machine.level)
+    const {customData, level} = machine
+    let rawOldSolidBlockPos = customData.getIntArray("oldSolidBlockPos")
+    let rawSolidBlockPos = customData.getIntArray("solidBlockPos")
+    let solidBlockPos = [rawSolidBlockPos[0], rawSolidBlockPos[1], rawSolidBlockPos[2]]
+    let oldSolidBlockPos = [rawOldSolidBlockPos[0], rawOldSolidBlockPos[1], rawOldSolidBlockPos[2]]
+    let manager = SeasonUtil.getSaveData(level)
     if (manager != null) {
-        manager.removeHumidityControlProvider(machine.pos)
+        manager.removeHumidityControlProvider(oldSolidBlockPos)
+        manager.removeHumidityControlProvider(solidBlockPos)
     }
 })
