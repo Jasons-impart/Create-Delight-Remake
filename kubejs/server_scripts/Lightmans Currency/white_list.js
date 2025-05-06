6999148fbb86acf0e8df26c420cd233e18a9c29f
SSWTLZZ69 <1824914068@qq.com>
2025-05-06 21:37:31 +0800

    加入一个出售矿物资源的商店（也需要解锁）

--- kubejs/server_scripts/Lightmans Currency/white_list.js
+++ kubejs/server_scripts/Lightmans Currency/white_list.js
@@ -5,10 +5,12 @@ ItemEvents.rightClicked("minecraft:stick", e => {
 /**
  * 
  * @param {Internal.QuestObjectCompletedEventJS} e 
- * @param {Internal.ItemStack_} item 
+ * @param {Internal.ItemStack_[]} item 
  */
-function unlockTechTrade(e, item) {
-    let trader = TradeUtil.getTradeAPI().GetTrader(false, 10)
+function unlockTraderTrade(e, id, item) {
+    if (!(item instanceof Array))
+        item = [item]
+    let trader = TradeUtil.getTradeAPI().GetTrader(false, id)
     let player = e.player
     trader.tradeData.forEach(tradeData => {
         tradeData.rules.forEach(rule => {
@@ -21,39 +23,54 @@ function unlockTechTrade(e, item) {
                 * @type {Internal.ItemTradeData}
                 */
                 let itemTradeData = tradeData
-                if (itemTradeData.getSellItem(0).is(item) || itemTradeData.getSellItem(1).is(item)) {
-                    player.tell(listRule.addToWhitelist(player))
-                    trader.markTradeRulesDirty()
-                }
+                item.forEach(item => {
+                    if (itemTradeData.getSellItem(0).is(item) || itemTradeData.getSellItem(1).is(item)) {
+                        if (listRule.addToWhitelist(player))
+                            trader.markTradeRulesDirty()
+                    }
+                })
             }
         })
     })
 }
 
-let list = [
-    ["510B2EE25982FD73", "create:andesite_alloy"],
-    ["0E515BE9EA2A0B2F", "create:brass_ingot"],
-    ["70CA8C3E0DEBBE1A", "createmetallurgy:steel_ingot"],
-    ["2CBBADBE62382C1B", "create:electron_tube"],
-    ["575185F2DB0AD44C", "create:precision_mechanism"],
-    ["20B3A29ADFDF1D4A", "createdelightcore:bronze_ingot"],
-    ["55E42ECE1CE4E99F", "createutilities:void_steel_ingot"],
-    ["2CC8B87D086B3275", "createaddition:electrum_ingot"],
-    ["614A05331AB5461E", "ae2:logic_processor"],
-    ["553A46B5D4145655", "ae2:calculation_processor"],
-    ["452DE0751B904A74", "megacells:accumulation_processor"],
-    ["6B3B3C96CDBD584F", "ae2:engineering_processor"],
-    ["292ADE53BBE862DB", "ae2:certus_quartz_crystal"],
-    ["4CBACEC15826AB34", "ae2:fluix_crystal"],
-    ["7A9629CF8E2BC6F1", "ae2:annihilation_core"],
-    ["4F6B70A38ACBA2AC", "ae2:formation_core"],
-    ["605F75F85D1E9394", "ad_astra:desh_ingot"],
-    ["393E1E2F429F9972", "ad_astra:ostrum_ingot"],
-    ["0EF849425CE2BC23", "ad_astra:calorite_ingot"]
+let tech_list = [
+    ["7D54AF3A775B6E9E", "create:andesite_alloy"],
+    ["7A608C48C3947975", "create:brass_ingot"],
+    ["027EEF0721C497D3", "createmetallurgy:steel_ingot"],
+    ["3A1374A5FCBE5558", "create:electron_tube"],
+    ["2C75BC13E58E3293", "create:precision_mechanism"],
+    ["7794F6910AF7591C", "createdelightcore:bronze_ingot"],
+    ["02FD263E4D2BFD82", "createutilities:void_steel_ingot"],
+    ["256EC76BA5595E4C", "createmetallurgy:obdurium_ingot"],
+    ["52CAEFBED4952D92", "createaddition:electrum_ingot"],
+    ["50B7170A5AAEBA8A", "ae2:logic_processor"],
+    ["1219B75C2EB5BDD3", "ae2:calculation_processor"],
+    ["555915045BD9EEC0", "megacells:accumulation_processor"],
+    ["5B0583F6E1474359", "ae2:engineering_processor"],
+    ["7781E14E3569EDC6", "ae2:certus_quartz_crystal"],
+    ["611F3C0D64351D0F", "ae2:fluix_crystal"],
+    ["6098CD2DFB98E121", "ae2:annihilation_core"],
+    ["3042E8CAEFF3F7CE", "ae2:formation_core"],
+    ["4FCBBE103FE7E9C7", "ad_astra:desh_ingot"],
+    ["057F6F421273C4DC", "ad_astra:ostrum_ingot"],
+    ["630E52E14877F5B7", "ad_astra:calorite_ingot"]
 ]
-list.forEach(v => {
+let res_list = [
+    ["63356DE1A41FB7E0", ["minecraft:copper_ingot", "minecraft:iron_ingot", "minecraft:coal", "createdelightcore:tin_ingot", "create:zinc_ingot"]],
+    ["441F72B76AC3D7AC", ["minecraft:gold_ingot", "minecraft:diamond", "iceandfire:silver_ingot"]],
+    ["36114A8D5283E6E5", ["createmetallurgy:tungsten_ingot", "minecraft:ancient_debris"]]
+]
+tech_list.forEach(v => {
     FTBQuestsEvents.completed(v[0], e => {
-        unlockTechTrade(e, v[1])
+        unlockTraderTrade(e, 10, v[1])
+        $TraderDataCache.TYPE.get(false).reloadPersistentTraders()
+    })
+})
+
+res_list.forEach(v => {
+    FTBQuestsEvents.completed(v[0], e => {
+        unlockTraderTrade(e, 14, v[1])
         $TraderDataCache.TYPE.get(false).reloadPersistentTraders()
     })
 })
