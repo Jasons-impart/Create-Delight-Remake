b77d2f0c2e7122aa5873664e29315d407bfff983
SSWTLZZ69 <1824914068@qq.com>
2025-05-14 13:27:14 +0800

    修改方块使用工具的的破坏效率；加入minemenu模组并增加一些按键 (#536)

--- kubejs/client_scripts/network.js
+++ kubejs/client_scripts/network.js
@@ -1,5 +1,6 @@
 
 const $FluidFX = Java.loadClass("com.simibubi.create.content.fluids.FluidFX")
+const $HoloGui = Java.loadClass("se.mickelus.tetra.items.modular.impl.holo.gui.HoloGui")
 NetworkEvents.dataReceived("kubejs_player_playsound", e => {
     e.player.playSound(e.data.get("soundEvent").getAsString())
 })
@@ -9,4 +10,10 @@ NetworkEvents.dataReceived("spawn_create_particle", e => {
     let particle = $FluidFX.getFluidParticle(Fluid.of(fluid).fluid)
     
     e.player.level.addParticle(particle, x, y, z, vx, vy, vz)
+})
+
+NetworkEvents.dataReceived("openholo", e => {
+    let gui = $HoloGui.getInstance()
+    Client.setScreen(gui)
+    gui.onShow()
 })
