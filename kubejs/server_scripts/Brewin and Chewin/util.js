ab25ab21ec1dc0ad2dd23f7a6eea65833903168e
SSWTLZZ69 <1824914068@qq.com>
2025-05-16 17:14:31 +0800

    加入brewin and chewin，并使vinery酒改为使用其发酵桶制作

---
+++ kubejs/server_scripts/Brewin and Chewin/util.js
@@ -0,0 +1,53 @@
+/**
+ * 
+ * @param {Internal.RecipesEventJS_} e 
+ * @param {OutputItem_ | Internal.OutputFluid_} result 
+ * @param {Internal.Ingredient[]} ingredients 
+ * @param {Internal.InputFluid_} inputFluid 
+ * @param {number} [experience]
+ * @param {number} [fermentingtime] 
+ * @param {number} [temperature]
+ * @returns 
+ */
+function fermenting(e, result, ingredients, inputFluid, experience, fermentingtime, temperature) {
+    let ingrs = []
+    if (!(ingredients instanceof Array)) {
+        ingredients = [ingredients]
+    }
+    ingredients.forEach(i => {
+        i.stacks.forEach(item => {
+            ingrs.push(Ingredient.of(item).toJson())
+        })
+    })
+    let res = {}
+    if (!result.amount) {
+        let tmp = Item.of(result)
+        res = {
+            count: tmp.count,
+            item: tmp.id
+        }
+    }
+    else {
+        res = {
+            count: result.amount,
+            fluid: result.id
+        }
+    }
+    inputFluid = Fluid.of(inputFluid)
+    fermentingtime = fermentingtime || 9600
+    experience = experience || 1.0
+    temperature = temperature || 3
+    let json = {
+        type: "brewinandchewin:fermenting",
+        basefluid: {
+            count: inputFluid.amount,
+            fluid: inputFluid.id
+        },
+        experience: experience,
+        fermentingtime: fermentingtime,
+        ingredients: ingrs,
+        result: res,
+        temperature: temperature
+    }
+    return e.custom(json)
+}
--- kubejs/server_scripts/util/respite.js
+++ kubejs/server_scripts/util/respite.js
@@ -90,16 +90,29 @@ function brewing_2(event, base, inputs, fluid, output, amount) {
  * @param {Internal.RecipesEventJS_} event 
  * @param {InputItem_} input 
  * @param {Internal.FluidStackJS_} fluid 
+ * @param {InputItem_} [container]
+ * @param {number} amount
  */
-function pouring(event, input, fluid) {
+function pouring(event, input, fluid, container, amount) {
     let ingr = Ingredient.of(input)
+    let con = container || "minecraft:glass_bottle"
+    amount = amount || 250
+    let c = Ingredient.of(con)
     event.custom({
         "type": "farmersrespite:kettle_pouring",
-        "amount": 250,
-        "container": {
-            "item": "minecraft:glass_bottle"
-        },
+        "amount": amount,
+        "container": c,
         "fluid": fluid,
         "output": ingr
     }).id(`farmersrespite:pouring/${ingr.getFirst().getId().split(":")[1]}`)
+
+    event.custom({
+        "type": "brewinandchewin:keg_pouring",
+        "amount": amount,
+        "container": c,
+        "filling": true,
+        "fluid": fluid,
+        "output": ingr,
+        "strict": false
+    })
 }
