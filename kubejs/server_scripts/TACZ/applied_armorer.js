cca76da963a362f565bcdeda6c2c21f2796d48a2
SSWTLZZ69 <1824914068@qq.com>
2025-05-24 00:28:20 +0800

    应用军械师配方编写完毕；修复枪械声音消失的问题

--- kubejs/server_scripts/TACZ/applied_armorer.js
+++ kubejs/server_scripts/TACZ/applied_armorer.js
@@ -3,16 +3,18 @@ ServerEvents.recipes(e => {
     /**
      * 
      * @param {OutputItem_} result 
-     * @param {InputItem_?} top 
-     * @param {InputItem_?} middle 
-     * @param {InputItem_?} bottom 
+     * @param {InputItem_} [top] 
+     * @param {InputItem_} [middle] 
+     * @param {InputItem_} [bottom] 
+     * @param {string} [mode]
      * @returns 
      */
-    let custom_inscribe = (result, top, middle, bottom) => {
+    let custom_inscribe = (result, top, middle, bottom, mode) => {
         top = Ingredient.of(top || "minecraft:air")
         middle = Ingredient.of(middle || "minecraft:air")
         bottom = Ingredient.of(bottom || "minecraft:air")
         result = Item.of(result || "minecraft:air")
+        mode = mode || "press"
         return e.custom({
             type: "ae2:inscriber",
             ingredients: {
@@ -20,7 +22,7 @@ ServerEvents.recipes(e => {
                 middle: middle,
                 bottom: bottom,
             },
-            mode: "inscribe",
+            mode: "press",
             result: result,
         })
     }
@@ -44,6 +46,29 @@ ServerEvents.recipes(e => {
             circumstance: {
                 type: "fluid",
                 tag: fluid
+            },
+            ingredients: ingredients,
+            result: res
+        })
+    }
+    /**
+ *  
+ * @param {OutputItem_} res 
+ * @param {Internal.Ingredient[]} ingr
+ * @returns 
+ */
+    let transform_explosion = (res, ingr) => {
+        res = Item.of(res || "minecraft:air")
+        if (!(ingr instanceof Array))
+            ingr = [ingr]
+        let ingredients = []
+        ingr.forEach(ing => {
+            ingredients.push(Ingredient.of(ing))
+        })
+        return e.custom({
+            type: "ae2:transform",
+            circumstance: {
+                type: "explosion"
             },
             ingredients: ingredients,
             result: res
@@ -269,32 +294,60 @@ ServerEvents.recipes(e => {
         D: "ae2:cell_component_16k"
     })
     kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_1"}'), [
-        " A ",
         "ABA",
-        " A "
+        "BCB",
+        "ABA"
     ], {
-        A: "ae2:energy_cell",
-        B: "megacells:accumulation_processor"
+        A: "#forge:gems/fluix",
+        B: "ae2:energy_cell",
+        C: "megacells:accumulation_processor"
     })
         .id("applied_armorer:extended_battery_aa_1")
     kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_2"}'), [
-        " A ",
         "ABA",
-        " A "
+        "BCB",
+        "ABA"
     ], {
-        A: "ae2:dense_energy_cell",
-        B: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_1"}').strongNBT()
+        A: "ae2:energy_card",
+        B: "ae2:dense_energy_cell",
+        C: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_1"}').strongNBT()
     })
         .id("applied_armorer:extended_battery_aa_2")
     kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_3"}'), [
-        " A ",
         "ABA",
-        " A "
+        "BCB",
+        "ABA"
     ], {
-        A: "megacells:mega_energy_cell",
-        B: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_2"}').strongNBT()
+        A: "megacells:greater_energy_card",
+        B: "megacells:mega_energy_cell",
+        C: Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_battery_aa_2"}').strongNBT()
     })
         .id("applied_armorer:extended_battery_aa_3")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_commander"}'), [
+        "AAA",
+        "   ",
+        "BAA"
+    ], {
+        A: "#forge:rods/iron",
+        B: "#forge:nuggets/iron"
+    }).id("applied_armorer:muzzle_commander")
+    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_bs_mod4"}'), [
+        "ABA",
+        "   ",
+        "ABA"
+    ], {
+        A: "#forge:rods/iron",
+        B: "#forge:nuggets/iron"
+    }).id("applied_armorer:muzzle_bs_mod4")
+    kubejs.shaped(Item.of('tacz:ammo', '{AmmoId:"applied_armorer:fluix_battery"}'), [
+        "ABA",
+        "ACA",
+        "AAA"
+    ], {
+        A: "megacells:sky_steel_ingot",
+        B: "#forge:gems/fluix",
+        C: Item.of('ae2:energy_cell', '{internalCurrentPower:200000.0d,internalMaxPower:200000.0d}').strongNBT()
+    }).id("applied_armorer:fluix_battery")
     custom_inscribe(
         Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_mid_mag_aa_1"}'),
         Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:extended_mag_aa_1"}').strongNBT(),
@@ -382,41 +435,99 @@ ServerEvents.recipes(e => {
         Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_chip_hyper_propellant"}').strongNBT(),
         "megacells:mega_energy_cell"
     ).id("applied_armorer:muzzle_chip_fat_boy")
-    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_commander"}'), [
-        "AAA",
-        "   ",
-        "BAA"
-    ], {
-        A: "#forge:rods/iron",
-        B: "#forge:nuggets/iron"
-    }).id("applied_armorer:muzzle_commander")
-    kubejs.shaped(Item.of('tacz:attachment', '{AttachmentId:"applied_armorer:muzzle_bs_mod4"}'), [
-        "ABA",
-        "   ",
-        "ABA"
-    ], {
-        A: "#forge:rods/iron",
-        B: "#forge:nuggets/iron"
-    }).id("applied_armorer:muzzle_bs_mod4")
-    transform_fluid(Item.of('tacz:ammo', 4, '{AmmoId:"applied_armorer:cluster_quartz_bullet"}'), "createdelight:spent_liquor", [
-        "#forge:dusts/ender_pearl",
-        "#forge:dusts/certus_quartz",
-        Item.of('tacz:ammo', 4, '{AmmoId:"tacz:12g"}').strongNBT()
-    ]).id("applied_armorer:cluster_quartz_bullet")
-    transform_fluid(Item.of('tacz:ammo', 4, '{AmmoId:"applied_armorer:hard_core_quartz_bullet"}'), "createdelight:spent_liquor", [
-        "#forge:dusts/ender_pearl",
-        "#forge:dusts/certus_quartz",
-        Item.of('tacz:ammo', 4, '{AmmoId:"create_armorer:rbapb"}').strongNBT()
-    ]).id("applied_armorer:hard_core_quartz_bullet")
-    transform_fluid(Item.of('tacz:ammo', 4, '{AmmoId:"applied_armorer:etched_quartz_bullet"}'), "createdelight:spent_liquor", [
-        "#forge:dusts/ender_pearl",
-        "#forge:dusts/certus_quartz",
-        Item.of('tacz:ammo', 4, '{AmmoId:"create_armorer:rbapb"}').strongNBT()
-    ]).id("applied_armorer:etched_quartz_bullet")
-    transform_fluid(Item.of('tacz:ammo', 4, '{AmmoId:"applied_armorer:fluix_infused_grenade"}'), "createdelight:spent_liquor", [
-        "#forge:dusts/ender_pearl",
-        "#forge:dusts/certus_quartz",
-        Item.of("protection_pixel:pneumaticgrenade", 4)
-    ]).id("applied_armorer:fluix_infused_grenade")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:niklas_pistol_semi_pride",HasBulletInBarrel:0b}'),
+        "ae2:annihilation_core",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_pistol_semi_union"}').weakNBT(),
+        "ae2:basic_card"
+    ).id("applied_armorer:niklas_pistol_semi_pride")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:niklas_pistol_semi_right",HasBulletInBarrel:0b}'),
+        "ae2:annihilation_core",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_pistol_semi_pride"}').weakNBT(),
+        "ae2:advanced_card"
+    ).id("applied_armorer:niklas_pistol_semi_right")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:niklas_pistol_double_win_win",HasBulletInBarrel:1b}'),
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_pistol_semi_union"}').weakNBT(),
+        "ae2:singularity",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_pistol_semi_union"}').weakNBT(),
+    ).id("applied_armorer:niklas_pistol_double_win_win")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:moritz_rifle_ar77",HasBulletInBarrel:0b}'),
+        "ae2:annihilation_core",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_smg_freedom"}').weakNBT(),
+        "ae2:basic_card"
+    ).id("applied_armorer:moritz_rifle_ar77")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:niklas_lever_vigenere",HasBulletInBarrel:0b}'),
+        "ae2:annihilation_core",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:moritz_rifle_ar77"}').weakNBT(),
+        "ae2:energy_card"
+    ).id("applied_armorer:niklas_lever_vigenere")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:moritz_shotgun_sg914",HasBulletInBarrel:0b}'),
+        "ae2:annihilation_core",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:moritz_rifle_ar77"}').weakNBT(),
+        "ae2:speed_card"
+    ).id("applied_armorer:moritz_shotgun_sg914")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:moritz_sniper_semi_k30",HasBulletInBarrel:0b}'),
+        "ae2:speed_card",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_lever_vigenere"}').weakNBT(),
+        "megacells:greater_energy_card"
+    ).id("applied_armorer:moritz_sniper_semi_k30")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:moritz_mg_hmg22",HasBulletInBarrel:0b}'),
+        "ae2:speed_card",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_smg_freedom"}').weakNBT(),
+        "ae2:energy_card"
+    ).id("applied_armorer:moritz_mg_hmg22")
+    custom_inscribe(
+        Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:moritz_gernade_gl3",HasBulletInBarrel:0b}'),
+        "megacells:compression_card",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:niklas_lever_vigenere"}').weakNBT(),
+        "ae2:energy_card"
+    ).id("applied_armorer:moritz_gernade_gl3")
+    
 
+    transform_fluid(Item.of('tacz:ammo', '{AmmoId:"applied_armorer:cluster_quartz_bullet"}'), "createdelight:spent_liquor", [
+        "#forge:dusts/ender_pearl",
+        "#forge:dusts/certus_quartz",
+        Item.of('tacz:ammo', '{AmmoId:"tacz:12g"}').strongNBT()
+    ]).id("applied_armorer:cluster_quartz_bullet")
+    transform_fluid(Item.of('tacz:ammo', '{AmmoId:"applied_armorer:hard_core_quartz_bullet"}'), "createdelight:spent_liquor", [
+        "#forge:dusts/ender_pearl",
+        "#forge:dusts/certus_quartz",
+        Item.of('tacz:ammo', '{AmmoId:"create_armorer:rbapb"}').strongNBT()
+    ]).id("applied_armorer:hard_core_quartz_bullet")
+    transform_fluid(Item.of('tacz:ammo', '{AmmoId:"applied_armorer:etched_quartz_bullet"}'), "createdelight:spent_liquor", [
+        "#forge:dusts/ender_pearl",
+        "#forge:dusts/certus_quartz",
+        Item.of('tacz:ammo', '{AmmoId:"create_armorer:rbapb"}').strongNBT()
+    ]).id("applied_armorer:etched_quartz_bullet")
+    transform_fluid(Item.of('tacz:ammo', '{AmmoId:"applied_armorer:fluix_infused_grenade"}'), "createdelight:spent_liquor", [
+        "#forge:dusts/ender_pearl",
+        "#forge:dusts/certus_quartz",
+        Item.of("protection_pixel:pneumaticgrenade")
+    ]).id("applied_armorer:fluix_infused_grenade")
+    transform_fluid(Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:niklas_pistol_semi_union",HasBulletInBarrel:0b}'), "createdelight:spent_liquor", [
+        "#forge:storage_blocks/sky_steel",
+        "#forge:storage_blocks/iron",
+        "ae2:singularity",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"create_armorer:pistol_auto_stress"}').weakNBT()
+    ]).id("applied_armorer:niklas_pistol_semi_union")
+    transform_fluid(Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"SEMI",GunId:"applied_armorer:niklas_smg_freedom",HasBulletInBarrel:0b}'), "createdelight:spent_liquor", [
+        "#forge:storage_blocks/sky_steel",
+        "#forge:storage_blocks/iron",
+        "ae2:singularity",
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"create_armorer:smg_auto_crank"}').weakNBT()
+    ]).id("applied_armorer:smg_auto_crank")
+    transform_explosion(Item.of('tacz:modern_kinetic_gun', '{GunCurrentAmmoCount:0,GunFireMode:"AUTO",GunId:"applied_armorer:moritz_mg_emg_prototype",HasBulletInBarrel:0b}'), [
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:moritz_mg_hmg22"}').weakNBT(),
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:moritz_mg_hmg22"}').weakNBT(),
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:moritz_mg_hmg22"}').weakNBT(),
+        Item.of('tacz:modern_kinetic_gun', '{GunId:"applied_armorer:moritz_mg_hmg22"}').weakNBT(),
+        "ae2:quantum_link"
+    ]).id("applied_armorer:moritz_mg_emg_prototype")
 })
