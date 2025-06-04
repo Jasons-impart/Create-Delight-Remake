cf8f1a3ed229686183cfdd2cad436d55db72d25b
SSWTLZZ69 <1824914068@qq.com>
2025-06-04 12:16:59 +0800

    为多方块结构加一个pondertag；调整一些乱七八糟的设置

--- kubejs/server_scripts/Dungeons Delight/misc.js
+++ kubejs/server_scripts/Dungeons Delight/misc.js
@@ -1,3 +1,5 @@
 ItemEvents.foodEaten("dungeonsdelight:soft_serve_sniffer_egg", e => {
-    e.player.give("trailandtales_delight:sniffer_eggshell")
+    e.server.scheduleInTicks(1, func => {
+        e.player.give("trailandtales_delight:sniffer_eggshell")
+    })
 })
