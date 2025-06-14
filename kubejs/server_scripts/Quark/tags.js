82380bfd807193b68f257768fe8a4efdcf3080d9
AzureCrab <wanquan.wu.ca@gmail.com>
2025-06-14 17:45:01 +0800

    [fix]更新客户端模组列表 - 齿轮麦穗改为官方版本 - 更新 colorwheel - 更新 输入法修复 - 更新 越肩视角 - 更新 simplebackup - 更新节气 pre4-7 -> pre7-2   - （https://github.com/joe-vettek/Ecliptic-Seasons/blob/forge-1.20.1-opt/changelog.md） - 更新modernfix - 更新curiouslaterns - 更新MEED - 更新tetracompat - 更新curiouslaterns   -（修复了纸灯笼挂身上崩溃的问题，因此让纸灯笼可以挂到腰带栏上） - 更新AE2 - 更新copycats - 更新齿轮麦穗 - 更新create structure arise - 更新节气 - 更新guideme - 更新网易云音乐 - 更新CreateBetterFPS - 更新SleepTight - 更新YeetuExperimentus - 移除没有被依赖的库模组 collective

--- kubejs/server_scripts/Quark/tags.js
+++ kubejs/server_scripts/Quark/tags.js
@@ -1,8 +1,4 @@
 ServerEvents.tags("item", e => {
-    e.remove("curios:belt", [
-        "quark:paper_lantern",
-        "quark:paper_lantern_sakura"
-    ])
     e.removeAllTagsFrom([
         'quark:potato_crate'
     ])
