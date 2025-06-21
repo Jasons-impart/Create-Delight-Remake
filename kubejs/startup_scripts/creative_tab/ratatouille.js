a5976e2496fa65ba5b2e43e0e9f5565ccf146175
JasonQ <154645632+JasonQ1123@users.noreply.github.com>
2025-06-21 13:17:17 +0800

    修复alexcave稀有度导致的偶发性崩溃问题，新增LCVP材质包，修改矿物开掘的部分drill设定，完善任务三大饮料，新增任务大口吃肉 (#601)

--- kubejs/startup_scripts/creative_tab/ratatouille.js
+++ kubejs/startup_scripts/creative_tab/ratatouille.js
@@ -1,5 +1,7 @@
 StartupEvents.modifyCreativeTab("ratatouille:base", e => {
   e.remove([
-    'ratatouille:salt'
+    'ratatouille:salt',
+    'ratatouille:compost_residue',
+    'ratatouille:boil_stone'
   ])
 })
