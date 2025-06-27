173bd0e11dca32fde709f6a557d436d6344774a4
SSWTLZZ69 <1824914068@qq.com>
2025-06-27 16:10:07 +0800

    修复Neapolitan牛奶炼药锅

--- kubejs/server_scripts/Neapolitan/bug_fix.js
+++ kubejs/server_scripts/Neapolitan/bug_fix.js
@@ -1,6 +1,19 @@
-BlockEvents.rightClicked("minecraft:cauldron", e => {
-  const{ player } = e
-  if(player.getMainHandItem().id == "minecraft:milk_bucket"){
+BlockEvents.rightClicked("neapolitan:milk_cauldron", e => {
+  let { player, block, hand } = e
+  if (!player.getItemInHand(hand).is("minecraft:glass_bottle"))
     e.cancel()
+  let prop = block.properties
+  let level = parseInt(block.properties.get("level"))
+  level--
+  if (level != 0) {
+    prop.put("level", level.toString())
+    block.set("neapolitan:milk_cauldron", prop)
   }
+  else {
+    block.set("minecraft:cauldron")
+  }
+  player.give("farmersdelight:milk_bottle")
+  player.getItemInHand(hand).shrink(1)
+  player.sendData("kubejs_player_playsound", {soundEvent: "minecraft:item.bottle.fill"})
+  e.cancel()
 })
