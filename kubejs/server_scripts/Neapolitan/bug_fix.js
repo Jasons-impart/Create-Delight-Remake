46ec85b4b6bd9ef4c748371a832d41c1ae7a301e
SSWTLZZ69 <1824914068@qq.com>
2025-06-27 19:35:32 +0800

    再修复装牛奶的炼药锅

--- kubejs/server_scripts/Neapolitan/bug_fix.js
+++ kubejs/server_scripts/Neapolitan/bug_fix.js
@@ -1,19 +1,38 @@
 BlockEvents.rightClicked("neapolitan:milk_cauldron", e => {
   let { player, block, hand } = e
-  if (!player.getItemInHand(hand).is("minecraft:glass_bottle"))
-    e.cancel()
   let prop = block.properties
   let level = parseInt(block.properties.get("level"))
-  level--
-  if (level != 0) {
-    prop.put("level", level.toString())
-    block.set("neapolitan:milk_cauldron", prop)
+  if (player.getItemInHand(hand).is("minecraft:glass_bottle")) {
+    level--
+    if (level != 0) {
+      prop.put("level", level.toString())
+      block.set("neapolitan:milk_cauldron", prop)
+    }
+    else {
+      block.set("minecraft:cauldron")
+    }
+    player.give("farmersdelight:milk_bottle")
+    player.getItemInHand(hand).shrink(1)
+    player.sendData("kubejs_player_playsound", { soundEvent: "minecraft:item.bottle.fill" })
   }
-  else {
-    block.set("minecraft:cauldron")
+  else if (player.getItemInHand(hand).is("farmersdelight:milk_bottle")) {
+    level++
+    if (level != 4) {
+      prop.put("level", level.toString())
+      block.set("neapolitan:milk_cauldron", prop)
+      player.getItemInHand(hand).shrink(1)
+      player.give("minecraft:glass_bottle")
+      player.sendData("kubejs_player_playsound", { soundEvent: "minecraft:item.bottle.fill" })
+    }
   }
-  player.give("farmersdelight:milk_bottle")
-  player.getItemInHand(hand).shrink(1)
-  player.sendData("kubejs_player_playsound", {soundEvent: "minecraft:item.bottle.fill"})
   e.cancel()
+})
+
+BlockEvents.rightClicked("minecraft:cauldron", e => {
+  let { player, block, hand } = e
+  if (player.getItemInHand(hand).is("farmersdelight:milk_bottle")) {
+    player.sendData("kubejs_player_playsound", { soundEvent: "minecraft:item.bottle.fill" })
+    block.set("neapolitan:milk_cauldron", { level: "1" })
+    e.cancel()
+  }
 })
