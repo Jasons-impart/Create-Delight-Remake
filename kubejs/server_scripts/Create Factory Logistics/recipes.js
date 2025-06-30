3c37cc4cc90d4207e60b66b989cefdfa6b5f6483
SSWTLZZ69 <1824914068@qq.com>
2025-06-30 15:27:34 +0800

    使cdc同步6.0.6（处于分支中）

--- kubejs/server_scripts/Create Factory Logistics/recipes.js
+++ kubejs/server_scripts/Create Factory Logistics/recipes.js
@@ -6,6 +6,7 @@ ServerEvents.recipes(e => {
 BlockEvents.rightClicked(e => {
   let item = e.player.getMainHandItem();
   if(item.id == "create_factory_logistics:network_link"){
+    e.cancel();
     e.player.getHeldItem().shrink(1);
   }
 })
