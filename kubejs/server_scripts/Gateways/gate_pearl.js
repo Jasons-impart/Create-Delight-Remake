a2f969ec87ff61d04144daa91345b211ac1e339f
cawyyds <14791698+cawyyds@user.noreply.gitee.com>
2025-07-09 20:19:47 +0800

    添加寂静空谷主题试炼，更改gateway任务文案及试炼合成和一些试炼数值

--- kubejs/server_scripts/Gateways/gate_pearl.js
+++ kubejs/server_scripts/Gateways/gate_pearl.js
@@ -44,13 +44,12 @@ ServerEvents.recipes(e => {
         create.cutting(incomplete, incomplete),
         vintageimprovements.vibrating(incomplete, incomplete),
         create.filling(incomplete, [incomplete, Fluid.of("createdelight:ice_dragon_blood", 250)]),
-        create.deploying(incomplete, [incomplete, "iceandfire:dread_shard"])
+        create.deploying(incomplete, [incomplete, "createdelight:dread_heart"])
     ]).transitionalItem(incomplete).loops(4)
     .id("createdelight:sequenced_assembly/the_gate_of_eternal_cold")
     //添加配方：枯萎穿刺试炼合成
-
     create.sequenced_assembly([
-        Item.of(Item.of('gateways:gate_pearl', '{gateway:"createdelight:piercing_withering_trial"}')).withChance(4.0),
+        Item.of(Item.of('gateways:gate_pearl', '{gateway:"createdelight:piercing_withering_trial"}')).withChance(2.0),
         Item.of("minecarft:ender_pearl").withChance(0.01),
     ], "minecraft:ender_pearl", [
         create.deploying(incomplete, [incomplete, "minecraft:wither_skeleton_skull"]),
@@ -58,10 +57,10 @@ ServerEvents.recipes(e => {
         vintageimprovements.vibrating(incomplete, incomplete),
         create.deploying(incomplete, [incomplete, "minecraft:nether_star"]),
         create.deploying(incomplete, [incomplete, "iceandfire:witherbone"]),
+        create.deploying(incomplete, [incomplete, "art_of_forging:sigil_of_eden"]),
         create.deploying(incomplete, [incomplete, "createdelight:otherworld_note"])
-    ]).transitionalItem(incomplete).loops(2)
+    ]).transitionalItem(incomplete).loops(4)
     .id("createdelight:sequenced_assembly/piercing_withering_trial")
-
     //添加恶魂试炼
     e.shaped(Item.of('gateways:gate_pearl', '{gateway:"createdelight:ghast_trial"}'), [
         "AAA",
@@ -71,4 +70,30 @@ ServerEvents.recipes(e => {
         A: "minecraft:ghast_tear",
         B: "minecraft:ender_pearl"
     })
+    //添加配方：糖分临界点合成
+    create.sequenced_assembly([
+        Item.of(Item.of('gateways:gate_pearl', '{gateway:"createdelight:sweettide_brokenpoint"}')).withChance(5.0),
+        Item.of("minecarft:ender_pearl").withChance(0.01),
+    ], "minecraft:ender_pearl", [
+        create.deploying(incomplete, [incomplete, "alexscaves:radiant_essence"]),
+        create.compacting(incomplete, incomplete),
+        vintageimprovements.vibrating(incomplete, incomplete),
+        create.deploying(incomplete, [incomplete, "alexscaves:music_disc_tasty"]),
+        create.deploying(incomplete, [incomplete, "alexscaves:conversion_crucible"]),
+        create.deploying(incomplete, [incomplete, "alexscaves:biome_treat"])
+    ]).transitionalItem(incomplete).loops(4)
+    .id("createdelight:sequenced_assembly/sweettide_brokenpoint")
+    ////添加配方：黯渊之视合成
+    create.sequenced_assembly([
+        Item.of(Item.of('gateways:gate_pearl', '{gateway:"createdelight:infinite_and_dark_trials"}')).withChance(7.0),
+        Item.of("minecarft:ender_pearl").withChance(0.01),
+    ], "minecraft:ender_pearl", [
+        create.deploying(incomplete, [incomplete, "alexscaves:pure_darkness"]),
+        create.compacting(incomplete, incomplete),
+        vintageimprovements.vibrating(incomplete, incomplete),
+        create.deploying(incomplete, [incomplete, "createdelight:devil_eye"]),
+        create.deploying(incomplete, [incomplete, "alexscaves:desolate_dagger"]),
+        create.deploying(incomplete, [incomplete, "alexscaves:dreadbow"])
+    ]).transitionalItem(incomplete).loops(2)
+    .id("createdelight:sequenced_assembly/infinite_and_dark_trials")
 })
