ad4424c531fdf06a0ff04819b8b1ed409f6f5e9f
JasonQ <154645632+JasonQ1123@users.noreply.github.com>
2025-07-10 14:09:06 +0800

    删除残留的熔融青铜配方 (#727)

--- kubejs/server_scripts/Create BigCannon/remove.js
+++ kubejs/server_scripts/Create BigCannon/remove.js
@@ -5,7 +5,8 @@ ServerEvents.recipes(e => {
         "createbigcannons:compacting/forge_nethersteel_ingot",
         "createbigcannons:mixing/alloy_nethersteel_cast_iron",
         "createbigcannons:compacting/forge_cast_iron_ingot",
-        "createbigcannons:compacting/forge_cast_iron_nugget"
+        "createbigcannons:compacting/forge_cast_iron_nugget",
+        "createbigcannons:compacting/forge_bronze_ingot"
     ])
     e.remove({type: "createbigcannons:melting"})
 })
