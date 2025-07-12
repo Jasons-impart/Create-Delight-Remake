3005a42e111574990b4c01c268820bdcd0d50540
JasonQ <nmuqindexiang@163.com>
2025-07-13 00:40:59 +0800

    修复琥珀金以及下界钢由于配方数量导致的左脚踩右脚问题，新增更多合金电炉适配合金配方，修改合金消耗电量使其合理

--- kubejs/server_scripts/mbd2_recipes/centrifugation.js
+++ kubejs/server_scripts/mbd2_recipes/centrifugation.js
@@ -58,7 +58,7 @@ ServerEvents.recipes(e => {
       [["createmetallurgy:molten_brass", 180], ["createmetallurgy:molten_copper", 90], ["createmetallurgy:molten_zinc", 90]],
       [["createmetallurgy:molten_bronze", 360], ["createmetallurgy:molten_tin", 90], ["createmetallurgy:molten_copper", 270]],
       [["createmetallurgy:molten_electrum", 180], ["createmetallurgy:molten_silver", 90], ["createmetallurgy:molten_gold", 90]],
-      [["createbigcannons:molten_nethersteel", 360], ["createmetallurgy:molten_steel", 90], ["createmetallurgy:molten_netherite", 90]],
+      [["createbigcannons:molten_nethersteel", 540], ["createmetallurgy:molten_steel", 90], ["createmetallurgy:molten_netherite", 90]],
       [["createdelight:vanilla_milkshake", 750], ["createdelightcore:vanilla_ice_cream", 250], ["minecraft:milk", 500]],
       [["createdelight:chocolate_milkshake", 750], ["createdelightcore:chocolate_ice_cream", 250], ["minecraft:milk", 500]],
       [["createdelight:strawberry_milkshake", 750], ["createdelightcore:strawberry_ice_cream", 250], ["minecraft:milk", 500]],
