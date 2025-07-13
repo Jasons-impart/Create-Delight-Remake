2e4daa1d97aa4d7963f8a84bb74910124d96b34f
AzureCrab <wanquan.wu.ca@gmail.com>
2025-07-13 23:31:29 +0800

    [fix]start.bat启动服务器不再借由run.bat，方便正确指定JAVA

--- start.bat
+++ start.bat
@@ -81,6 +81,7 @@ if not exist server.properties (
 
 @REM launch server
 echo Launching server...
-run.bat
+%JAVA% @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.0/win_args.txt %*
+pause
 
 endlocal
