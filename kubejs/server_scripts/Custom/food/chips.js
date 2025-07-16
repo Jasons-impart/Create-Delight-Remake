ec90a5a27c472255c985f10676780ecb6d9818b6
SSWTLZZ69 <1824914068@qq.com>
2025-07-16 13:02:16 +0800

    兼容四海一家和四季风味的配方

--- kubejs/server_scripts/Custom/food/chips.js
+++ kubejs/server_scripts/Custom/food/chips.js
@@ -3,5 +3,6 @@ ServerEvents.recipes(e => {
         "casualness_delight:cutting/potato_slice"
     ])
     cutting(e, "casualness_delight:potato_slice", [["create_bic_bit:raw_fries"]])
-    cutting(e, "minecraft:potato", [["casualness_delight:potato_slice"]])
+    cutting(e, "cosmopolitan:cut_potatoes", [["casualness_delight:potato_slice"]])
+    cutting(e, "minecraft:potato", [["cosmopolitan:cut_potatoes"]])
 })
