623038b8b003343ca77d708451ebe8ed39a56aee
SSWTLZZ69 <1824914068@qq.com>
2025-07-27 01:27:24 +0800

    调整盔甲值计算方式（高盔甲韧性对于高伤害有额外防御效果）；增加烈焰和冰冻蛋糕胚的产出；龙现在不会持有武器；降低所有保险库的造价 (#810)

--- kubejs/server_scripts/Create Better Storage/recipe.js
+++ kubejs/server_scripts/Create Better Storage/recipe.js
@@ -1,8 +1,5 @@
 ServerEvents.recipes(e => {
-    remove_recipes_output(e, [
-        "create_bs:wooden_item_vault",
-        "create_bs:netherite_item_vault"
-    ])
+    e.remove({mod: "create_bs"})
     const {kubejs} = e.recipes
     kubejs.shaped("3x create_bs:wooden_item_vault", [
         "ABA",
@@ -13,14 +10,116 @@ ServerEvents.recipes(e => {
         B: "createdieselgenerators:wood_chip",
         C: "minecraft:barrel"
     })
-    kubejs.shaped("create_bs:netherite_item_vault", [
+    kubejs.shaped("5x create_bs:copper_item_vault", [
         "ABA",
-        "CDC",
+        "BAB",
         "ABA"
     ], {
-        A: "minecraft:gold_block",
-        B: "minecraft:diamond_block",
-        C: "minecraft:netherite_ingot",
-        D: "create_bs:diamond_item_vault"
+        A: "create:item_vault",
+        B: "#forge:storage_blocks/copper"
     })
+    .id("create_bs:crafting/copper_item_vault")
+    
+    kubejs.shaped("5x create_bs:iron_item_vault", [
+        "ABA",
+        "BAB",
+        "ABA"
+    ], {
+        A: "create:item_vault",
+        B: "#forge:storage_blocks/iron"
+    })
+    .id("create_bs:crafting/iron_item_vault")
+    kubejs.shaped("5x create_bs:iron_item_vault", [
+        "ABA",
+        "CAC",
+        "ABA"
+    ], {
+        A: "create_bs:copper_item_vault",
+        B: "#forge:ingots/iron",
+        C: "#forge:storage_blocks/iron"
+    })
+    .id("create_bs:crafting/alt_iron_item_vault")
+
+    kubejs.shaped("5x create_bs:gold_item_vault", [
+        "ABA",
+        "BAB",
+        "ABA"
+    ], {
+        A: "create_bs:iron_item_vault",
+        B: "#forge:storage_blocks/gold"
+    })
+    .id("create_bs:crafting/gold_item_vault")
+    kubejs.shaped("5x create_bs:gold_item_vault", [
+        "ABA",
+        "CAC",
+        "ABA"
+    ], {
+        A: "create_bs:emerald_item_vault",
+        B: "#forge:ingots/gold",
+        C: "#forge:storage_blocks/gold"
+    })
+    .id("create_bs:crafting/alt_gold_item_vault")
+
+    kubejs.shaped("5x create_bs:emerald_item_vault", [
+        "ABA",
+        "BAB",
+        "ABA"
+    ], {
+        A: "create_bs:iron_item_vault",
+        B: "#forge:storage_blocks/emerald"
+    })
+    .id("create_bs:crafting/emerald_item_vault")
+    
+    kubejs.shaped("5x create_bs:crystal_item_vault", [
+        "ABA",
+        "CAC",
+        "ABA"
+    ], {
+        A: "create_bs:gold_item_vault",
+        B: "#forge:glass",
+        C: "#forge:storage_blocks/diamond"
+    })
+    .id("create_bs:crafting/crystal_item_vault")
+    
+    kubejs.shaped("5x create_bs:diamond_item_vault", [
+        "ABA",
+        "BAB",
+        "ABA"
+    ], {
+        A: "create_bs:gold_item_vault",
+        B: "#forge:storage_blocks/diamond"
+    })
+    .id("create_bs:crafting/diamond_item_vault")
+
+    kubejs.shaped("5x create_bs:diamond_item_vault", [
+        "ABA",
+        "BAB",
+        "ABA"
+    ], {
+        A: "create_bs:crystal_item_vault",
+        B: "#forge:gems/diamond"
+    })
+    .id("create_bs:crafting/alt_diamond_item_vault")
+    
+    
+    kubejs.shaped("5x create_bs:obsidian_item_vault", [
+        "ABA",
+        "BAB",
+        "ABA"
+    ], {
+        A: "create_bs:diamond_item_vault",
+        B: "#forge:obsidian"
+    })
+    .id("create_bs:crafting/obsidian_item_vault")
+    
+    kubejs.shaped("5x create_bs:netherite_item_vault", [
+        "ABA",
+        "CAC",
+        "ABA"
+    ], {
+        A: "create_bs:diamond_item_vault",
+        B: "#forge:ingots/netherite",
+        C: "#forge:storage_blocks/gold"
+    })
+    .id("create_bs:crafting/netherite_item_vault")
 })
