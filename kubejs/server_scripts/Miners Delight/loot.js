6fd9e083f9d0fb0d071bf5a2b29d8b192d02e7f2
AzureCrab <wanquan.wu.ca@gmail.com>
2025-08-02 23:17:28 +0800

    [fix]鱿鱼料理线物品全整理，顺便移除几个重复矿粒配方

--- kubejs/server_scripts/Miners Delight/loot.js
+++
@@ -1,5 +0,0 @@
-LootJS.modifiers(e => {
-    // e.addLootTypeModifier("fishing")
-    // .replaceLoot("miners_delight:squid", "culturaldelights:squid")
-    // .replaceLoot("miners_delight:glow_squid", "culturaldelights:glow_squid")
-})
