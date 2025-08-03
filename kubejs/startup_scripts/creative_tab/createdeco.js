eb70f53dce240a2a435ed456f68885207a36cf31
AzureCrab <wanquan.wu.ca@gmail.com>
2025-08-04 04:20:01 +0800

    [item merge][refactor]重构OEI数据包，增加货币合并，增加铁、铜、熔融青铜/钢合并

---
+++ kubejs/startup_scripts/creative_tab/createdeco.js
@@ -0,0 +1,21 @@
+StartupEvents.modifyCreativeTab("createdeco:props_tab", e => {
+  e.remove([
+    'createdeco:industrial_iron_coin',
+    'createdeco:zinc_coin',
+    'createdeco:brass_coin',
+    'createdeco:iron_coinstack',
+    'createdeco:copper_coinstack',
+    'createdeco:industrial_iron_coinstack',
+    'createdeco:zinc_coinstack',
+    'createdeco:gold_coinstack',
+    'createdeco:netherite_coinstack',
+    'createdeco:brass_coinstack',
+    'createdeco:netherite_nugget',
+    "createdeco:andesite_hull",
+    'createdeco:brass_hull',
+    'createdeco:iron_hull',
+    'createdeco:copper_hull',
+    'createdeco:industrial_iron_hull',
+    'createdeco:zinc_hull'
+  ])
+})
