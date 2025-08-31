354190296c6d137b03b02dd7babb5d8a996d4da9
JasonQ <154645632+JasonQ1123@users.noreply.github.com>
2025-09-01 00:20:00 +0800

    奶酪OEI合并修复之前不合理问题 (#928)

--- kubejs/server_scripts/Create BitterBallen/recipe.js
+++ kubejs/server_scripts/Create BitterBallen/recipe.js
@@ -243,4 +243,15 @@ ServerEvents.tags("minecraft:item", e => {
     e.add("forge:mayonnaise", [
         'create_bic_bit:mayonnaise_bottle'
     ])
+    e.removeAllTagsFrom([
+    'create_bic_bit:unripe_cheese',
+    'create_bic_bit:waxed_unripe_cheese',
+    'create_bic_bit:young_cheese',
+    'create_bic_bit:waxed_young_cheese',
+    'create_bic_bit:aged_cheese',
+    'create_bic_bit:waxed_aged_cheese',
+    'create_bic_bit:unripe_cheese_wedge',
+    'create_bic_bit:young_cheese_wedge',
+    'create_bic_bit:aged_cheese_wedge'
+    ])
 })
