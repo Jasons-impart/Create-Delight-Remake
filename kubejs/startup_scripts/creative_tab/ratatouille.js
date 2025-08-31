5f7d5c1c53b90c50eb4f57ff921773c8d1d1137f
JasonQ <nmuqindexiang@163.com>
2025-08-31 11:28:59 +0800

    增加Squareful-CDR，实现更多XK材质兼容，恢复ratatouille灌溉塔，删除烤箱tooltip，回退妖怪归家版本以修复世界生成bug

--- kubejs/startup_scripts/creative_tab/ratatouille.js
+++ kubejs/startup_scripts/creative_tab/ratatouille.js
@@ -2,6 +2,5 @@ StartupEvents.modifyCreativeTab("ratatouille:base", e => {
   e.remove([
     'ratatouille:compost_residue',
     'ratatouille:boil_stone',
-    "ratatouille:irrigation_tower",
   ])
 })
