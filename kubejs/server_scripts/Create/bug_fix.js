8470e84eb59acdbcba881ca96ad6f05ab9f75742
JasonQ <nmuqindexiang@163.com>
2025-09-25 00:09:06 +0800

    修复了衣壳体刷物品的BUG，汉化functionalstorage

--- kubejs/server_scripts/Create/bug_fix.js
+++ kubejs/server_scripts/Create/bug_fix.js
@@ -1,12 +1,35 @@
-// BlockEvents.rightClicked("alexsmobs:capsid", e => {
-//     const { block, player, level } = e
-//     if (block.entityData["Items"][0]["id"] == "create:minecart_contraption" && player.mainHandItem.id == "create:minecart_contraption") {
-//         e.cancel()
-//     }
-//     if (block.entityData["Items"][0]["id"].search("present") != -1 && player.mainHandItem.id.search("present") != -1) {
-//         e.cancel()
-//     }
-// })
+BlockEvents.rightClicked("alexsmobs:capsid", e => {
+    const { block, player, level } = e
+    let mainHandItem = player.mainHandItem
+    let entityData = block.entityData
+    if (
+        entityData["Items"][0]["id"] == "create:minecart_contraption" &&
+        mainHandItem.id == "create:minecart_contraption"
+    ) {
+        player.swing()
+        block.set("air")
+        block.set("alexsmobs:capsid")
+        e.cancel()
+    }
+    if (
+        entityData["Items"][0]["id"].search("present") != -1 &&
+        mainHandItem.id.search("present") != -1
+    ) {
+        player.swing()
+        block.set("air")
+        block.set("alexsmobs:capsid")
+        e.cancel()
+    }
+    if (
+        entityData["Items"][0]["id"].search("functionalstorage") != -1 &&
+        mainHandItem.id.search("functionalstorage") != -1
+    ) {
+        player.swing()
+        block.set("air")
+        block.set("alexsmobs:capsid")
+        e.cancel()
+    }
+})
 ServerEvents.tags("block", e => {
     e.remove("create:safe_nbt", [
         "create:clipboard"
