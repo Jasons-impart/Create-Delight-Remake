87622e16247a514cf575527725427d681575879a
AzureCrab <wanquan.wu.ca@gmail.com>
2025-09-25 23:31:33 +0800

    [recipe]简化齿轮麦穗催熟素合成，改为骨粉+生物质工作台合成

--- kubejs/server_scripts/ratatouille/recipes.js
+++ kubejs/server_scripts/ratatouille/recipes.js
@@ -4,4 +4,27 @@ ServerEvents.recipes(e => {
     "create:mixing/salt",
     "ratatouille:threshing/boil_stone"
   ])
+  remove_recipes_input(e, [
+    'ratatouille:ripen_matter',
+    'ratatouille:ripen_matter_fold',
+    'ratatouille:compost_mass',
+    'ratatouille:bio_gas_bucket',
+    'ratatouille:compost_tea_bucket',
+    'ratatouille:bio_gas',
+    'ratatouille:compost_tea',
+    'ratatouille:compost_residue_fluid',
+  ])
+  remove_recipes_output(e, [
+    'ratatouille:ripen_matter',
+    'ratatouille:ripen_matter_fold',
+    'ratatouille:compost_mass',
+    'ratatouille:bio_gas_bucket',
+    'ratatouille:compost_tea_bucket',
+    'ratatouille:bio_gas',
+    'ratatouille:compost_tea',
+    'ratatouille:compost_residue_fluid',
+  ])
+
+  // 上面删除催熟素生产的相关配方，改为骨粉+生物质合成催熟素
+  e.shapeless('ratatouille:ripen_matter', ['minecraft:bone_meal', 'createaddition:biomass'])
 })
