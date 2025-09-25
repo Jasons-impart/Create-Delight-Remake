9f25704c4381a4921c742581cec79b6a2144d8ab
AzureCrab <wanquan.wu.ca@gmail.com>
2025-09-25 23:32:33 +0800

    [recipe]简化齿轮麦穗催熟素合成，改为骨粉+生物质工作台合成 (#975)

--- kubejs/server_scripts/Crate Delight/recipe.js
+++ kubejs/server_scripts/Crate Delight/recipe.js
@@ -2,4 +2,15 @@ ServerEvents.recipes(e => {
     remove_recipes_id(e, [
         "cratedelight:potato_crate_smelting_fd"
     ])
+
+    e.recipes.kubejs.shaped("createdelight:mechanic_grinding_wheel", [
+        "AAA",
+        "ACB",
+        "AAA"
+    ], {
+        A: "create:andesite_alloy",
+        B: "create:shaft",
+        C: "minecraft:grindstone"
+    })
+        .id("createdelight:mechanic_grinding_wheel")
 })
--- kubejs/startup_scripts/creative_tab/ratatouille.js
+++ kubejs/startup_scripts/creative_tab/ratatouille.js
@@ -2,5 +2,13 @@ StartupEvents.modifyCreativeTab("ratatouille:base", e => {
   e.remove([
     'ratatouille:compost_residue',
     'ratatouille:boil_stone',
+    'ratatouille:ripen_matter',
+    'ratatouille:ripen_matter_fold',
+    'ratatouille:compost_mass',
+    'ratatouille:bio_gas_bucket',
+    'ratatouille:compost_tea_bucket',
+    'ratatouille:bio_gas',
+    'ratatouille:compost_tea',
+    'ratatouille:compost_residue_fluid',
   ])
 })
