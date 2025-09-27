a5590aed2cb688043275f6eeed9244a77bec8a22
AzureCrab <wanquan.wu.ca@gmail.com>
2025-09-27 22:55:02 +0800

    [dev][refactor]衣壳体刷物品bug修复代码改用不会访问空值的写法 (#988)

--- kubejs/server_scripts/Create/bug_fix.js
+++ kubejs/server_scripts/Create/bug_fix.js
@@ -1,34 +1,39 @@
 BlockEvents.rightClicked("alexsmobs:capsid", e => {
-    const { block, player, level } = e
-    let mainHandItem = player.mainHandItem
-    let entityData = block.entityData
-    if (
-        entityData["Items"][0]["id"] == "create:minecart_contraption" &&
-        mainHandItem.id == "create:minecart_contraption"
-    ) {
-        player.swing()
-        block.set("air")
-        block.set("alexsmobs:capsid")
-        e.cancel()
-    }
-    if (
-        entityData["Items"][0]["id"].search("present") != -1 &&
-        mainHandItem.id.search("present") != -1
-    ) {
-        player.swing()
-        block.set("air")
-        block.set("alexsmobs:capsid")
-        e.cancel()
-    }
-    if (
-        entityData["Items"][0]["id"].search("functionalstorage") != -1 &&
-        mainHandItem.id.search("functionalstorage") != -1
-    ) {
-        player.swing()
-        block.set("air")
-        block.set("alexsmobs:capsid")
-        e.cancel()
-    }
+  const { block, player, level } = e
+  let mainHandItem = player.mainHandItem
+  let entityData = block.entityData
+  let entityId = entityData 
+                  && entityData["Items"] 
+                  && entityData["Items"][0]
+                  && entityData["Items"][0]["id"] 
+                  || 'unknown';
+  if (
+    entityId == "create:minecart_contraption"
+    && mainHandItem?.id == "create:minecart_contraption"
+  ) {
+      player.swing()
+      block.set("air")
+      block.set("alexsmobs:capsid")
+      e.cancel()
+  }
+  if (
+    entityId.search("present") != -1
+    && mainHandItem?.id.search("present") != -1
+  ) {
+      player.swing()
+      block.set("air")
+      block.set("alexsmobs:capsid")
+      e.cancel()
+  }
+  if (
+    entityId.search("functionalstorage") != -1
+    && mainHandItem?.id.search("functionalstorage") != -1
+  ) {
+    player.swing()
+    block.set("air")
+    block.set("alexsmobs:capsid")
+    e.cancel()
+  }
 })
 ServerEvents.tags("block", e => {
     e.remove("create:safe_nbt", [
