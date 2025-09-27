64edb538b1699aba20e6d291c0ef9cf823fc374a
AzureCrab <wanquan.wu.ca@gmail.com>
2025-09-27 22:53:52 +0800

    [dev][refactor]衣壳体刷物品bug修复代码改用不会访问空值的写法

--- kubejs/client_scripts/bug_fix.js
+++ kubejs/client_scripts/bug_fix.js
@@ -2,23 +2,28 @@ BlockEvents.rightClicked("alexsmobs:capsid", e => {
   const { block, player, level } = e
   let mainHandItem = player.mainHandItem
   let entityData = block.entityData
+  let entityId = entityData 
+                  && entityData["Items"] 
+                  && entityData["Items"][0]
+                  && entityData["Items"][0]["id"]
+                  || 'unknown';
   if (
-    entityData["Items"][0]["id"] == "create:minecart_contraption" &&
-    mainHandItem.id == "create:minecart_contraption"
+    entityId == "create:minecart_contraption"
+    && mainHandItem?.id == "create:minecart_contraption"
   ) {
     e.cancel()
     player.sendInventoryUpdate()
   }
   if (
-    entityData["Items"][0]["id"].search("present") != -1 &&
-    mainHandItem.id.search("present") != -1
+    entityId.search("present") != -1
+    && mainHandItem?.id.search("present") != -1
   ) {
     e.cancel()
     player.sendInventoryUpdate()
   }
   if (
-    entityData["Items"][0]["id"].search("functionalstorage") != -1 &&
-    mainHandItem.id.search("functionalstorage") != -1
+    entityId.search("functionalstorage") != -1
+    && mainHandItem?.id.search("functionalstorage") != -1
   ) {
     e.cancel()
     player.sendInventoryUpdate()
