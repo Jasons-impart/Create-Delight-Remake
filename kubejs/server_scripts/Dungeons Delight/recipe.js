2926c1f41ef5d9a28311e0f59eefbab7d9bbee3b
JasonQ <nmuqindexiang@163.com>
2025-10-05 12:27:21 +0800

    将需要小麦的配方转化为麦粒/草杆以实现深度加工模拟

--- kubejs/server_scripts/Dungeons Delight/recipe.js
+++ kubejs/server_scripts/Dungeons Delight/recipe.js
@@ -10,4 +10,13 @@ ServerEvents.recipes(e => {
          ["trailandtales_delight:cooked_sniffer_egg_block", Fluid.of("cosmopolitan:adzuki_ice_cream", 1000)])
          .id("dungeonsdelight:filling/soft_serve_sniffer_egg")
     cutting_2(e, "dungeonsdelight:ghast_calamari", [["mynethersdelight:ghasta"]])
+    e.recipes.ratatouille.threshing(
+        [
+            "dungeonsdelight:wormroot_tendrils",
+            Item.of("2x minecraft:bone_meal").withChance(0.6),
+            Item.of("2x minecraft:slime_ball").withChance(0.4),
+            "2x farmersdelight:straw"
+        ],
+        "dungeonsdelight:gunk", 400
+    ).id("dungeonsdelight:threshing/gunk")
 })
