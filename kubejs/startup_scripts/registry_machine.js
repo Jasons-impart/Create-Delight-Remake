5d0871862b564778e9eb97881d0bbfcd86e5e756
AzureCrab <wanquan.wu.ca@gmail.com>
2025-10-08 22:39:19 +0800

    [feat]将应急工业平台替换为工业平台模组的 (#1022)
    * [feat]将应急工业平台替换为模组的
    
    * [ftbquest]补一句说明

--- kubejs/startup_scripts/registry_machine.js
+++ kubejs/startup_scripts/registry_machine.js
@@ -3,12 +3,6 @@ StartupEvents.registry("block", (event) => {
 
     let machinery = [
         // "electrolyzer",
-        "emergency_industrial_platform",
-        "emergency_industrial_platform_block",
-        "emergency_industrial_platform_lime",
-        "emergency_industrial_platform_lime_block",
-        "emergency_industrial_platform_dark",
-        "emergency_industrial_platform_dark_block"
     ]
     machinery.forEach((name) => {
         event.create(MODID + name, "custommachinery")
