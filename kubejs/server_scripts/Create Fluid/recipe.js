54e892076b27b5aa0096d1da096f7a37f2701772
SSWTLZZ69 <1824914068@qq.com>
2025-10-21 23:07:19 +0800

    降低外骨骼和背包套娃度；为水力引擎加一个前期配方

--- kubejs/server_scripts/Create Fluid/recipe.js
+++ kubejs/server_scripts/Create Fluid/recipe.js
@@ -1,5 +1,20 @@
 ServerEvents.recipes(e => {
-    const {create} = e.recipes
+    const {create, kubejs} = e.recipes
     e.replaceInput({id: "fluid:centrifugal_pump"}, "create:propeller", "create_sa:hydraulic_engine")
+    e.replaceInput({id: "fluid:mechanical_pipette"}, "create:precision_mechanism", "create_sa:hydraulic_engine")
+    remove_recipes_id(e, [
+        "fluid:mechanical_pipette"
+    ])
 
+    kubejs.shaped("fluid:pipette", [
+        "AAB",
+        "AC ",
+        "DE "
+    ], {
+        A: "create:brass_sheet",
+        B: "minecraft:copper_ingot",
+        C: "#forge:spring/between_500_2_1000",
+        D: "create_sa:hydraulic_engine",
+        E: "create:brass_casing"
+    }).id("fluid:mechanical_pipette")
 })
