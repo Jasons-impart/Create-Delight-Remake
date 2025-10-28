1f4d03d2b464f43492883959d28c5c0c8be128f3
SSWTLZZ69 <1824914068@qq.com>
2025-10-28 22:03:54 +0800

    修复厨锅出品质不正常的问题

--- kubejs/client_scripts/crop_tooltip.js
+++ kubejs/client_scripts/crop_tooltip.js
@@ -2,7 +2,9 @@ ItemEvents.tooltip(e => {
     e.addAdvancedToAll((item, advanced, text) => {
         /**@type {Internal.Stream<Internal.TagKey<Internal.Item>>} */
         let tags = item.getTags()
-        if (item.nbt.contains("quality_food")) {
+        if (tags == null)
+            return
+        if (item.hasNBT() && item.nbt.contains("quality_food")) {
             if (tags.anyMatch(tag => tag.location().toString().startsWith("eclipticseasons")))
                 text.add(Text.translate("tooltip.createdelight.quality_crop_grow"))
             if (item.edible)
