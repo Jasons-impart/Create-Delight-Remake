e03ba73985b858fc056e3e37cbbd631d91eb14d2
SSWTLZZ69 <1824914068@qq.com>
2025-10-28 10:42:48 +0800

    更改版本号 (#1128)

--- start.bat
+++ start.bat
@@ -81,7 +81,7 @@ if not exist server.properties (
 
 @REM launch server
 echo Launching server...
-%JAVA% @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.0/win_args.txt %*
+%JAVA% @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.10/win_args.txt %*
 pause
 
 endlocal
