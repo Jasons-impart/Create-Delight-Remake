19d94a3ae2315e2453220ff3ed2b5c7538e41945
JasonQ <nmuqindexiang@163.com>
2025-10-31 20:38:08 +0800

    修复某个傻逼bug（下界之星在敲龙蛋的时候会消耗）

--- kubejs/server_scripts/End's Delight/recipes.js
+++ kubejs/server_scripts/End's Delight/recipes.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes(e => {
     remove_recipes_id(e, [
        "ends_delight:food/chorus_fruit_milk_tea",
-       "ends_delight:food/chorus_cookie"
+       "ends_delight:food/chorus_cookie",
+       "ends_delight:crack_non_hatchable_dragon_egg"
     ])
     e.recipes.create.mixing(Fluid.of("create_central_kitchen:chorus_fruit_wine", 250), [
         "minecraft:sugar", Ingredient.of("#forge:chorus_fruits", 2)
@@ -36,4 +37,12 @@ ServerEvents.recipes(e => {
         .id(`ends_delight:emptying/bubble_tea`) 
 
     brewing(e, "create_central_kitchen:dragon_breath", ["minecraft:sugar"], "createdelight:dragon_breath_soda", 'ends_delight:dragon_breath_soda')
+    e.recipes.farmersdelight.cutting(
+        "#forge:dragonegg",
+        "#minecraft:axes",
+        [
+            'ends_delight:liquid_dragon_egg',
+            'ends_delight:half_dragon_egg_shell'
+        ]
+    ).id("ends_delight:cutting/dragon_egg_manual_only")
 })
