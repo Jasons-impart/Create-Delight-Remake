cac3bb23de180f7d14346b9fd5bad46a5d4f575e
SSWTLZZ69 <1824914068@qq.com>
2025-11-03 16:50:07 +0800

    上传寿司增加种类的方法（我不确定能不能用）

--- kubejs/server_scripts/Youkai's Homecoming/sushi.js
+++ kubejs/server_scripts/Youkai's Homecoming/sushi.js
@@ -1,3 +1,30 @@
+const $TableItemManager = Java.loadClass("dev.xkmc.youkaishomecoming.content.pot.table.item.TableItemManager")
+
+
+let SushiTable = {}
+// /**
+//  * 
+//  * @param {Internal.VariantModelPart} part 
+//  * @param {string} id 
+//  * @param {Internal.ItemStack_} stack 
+//  * @returns {Internal.VariantModelPart$Entry} 来自$TableItemManager
+//  */
+// SushiTable.addMappingFromItemStack = function(part, id, stack) {
+//   return part["addMapping(java.lang.String,net.minecraft.world.level.ItemLike)"](id, Item.of(stack).item)
+// }
+
+/**
+ * 
+ * @param {Internal.VariantModelPart} part 
+ * @param {string} id 
+ * @param {Internal.Ingredient_} ingrdient 
+ * @returns {Internal.VariantModelPart$Entry} 来自$TableItemManager
+ */
+SushiTable.addMappingFromIngredient = function(part, id, ingrdient) {
+  return part["addMapping(java.lang.String,java.util.function.Supplier)"](id, () => Ingredient.of(ingrdient))
+}
+
+// SushiTable.addMappingFromIngredient($TableItemManager.SUSHI_TOP, "sculk_sensor_tendril", "silentsdelight:sculk_sensor_tendril") //用例
 ServerEvents.recipes(e => {
   const{youkaishomecoming} = e.recipes
   youkaishomecoming.cuisine_mixed(
