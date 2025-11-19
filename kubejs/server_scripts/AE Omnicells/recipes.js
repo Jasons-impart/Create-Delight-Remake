bfd1b557ba40aff214a14b9d49e2d8e84c7157ae
AzureCrab <wanquan.wu.ca@gmail.com>
2025-11-19 22:47:46 +0800

    [recipe]增加末影钢充电配方；修复钢制玻璃机壳和通透机壳配方不是无序的问题 (#1226)

--- kubejs/server_scripts/AE Omnicells/recipes.js
+++ kubejs/server_scripts/AE Omnicells/recipes.js
@@ -1,3 +1,7 @@
 ServerEvents.recipes(e => {
-    
+  e.recipes.create_new_age.energising(
+    "createutilities:void_steel_ingot",
+    "ae2omnicells:charged_ender_ingot",
+    3200
+  ).id("create:energising/charged_ender_ingot")
 })
