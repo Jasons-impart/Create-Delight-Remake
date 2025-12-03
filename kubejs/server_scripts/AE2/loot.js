e0e3ffbcbe813f6c08179675d5b7d05e1be19a22
JasonQ <nmuqindexiang@163.com>
2025-12-03 22:35:38 +0800

    删除某个无用mod，修复ae2神秘方块掉落全部压印模板的bug，为ae2wtlib增加贴图

---
+++ kubejs/server_scripts/AE2/loot.js
@@ -0,0 +1,7 @@
+LootJS.modifiers(e => {
+  e.addBlockLootModifier("ae2:mysterious_cube")
+    .removeLoot("megacells:accumulation_processor_press")
+    .removeLoot("ae2omnicells:omni_link_print_press")
+    .removeLoot("ae2omnicells:complex_link_print_press")
+    .removeLoot("ae2omnicells:multidimensional_expansion_print_press")
+})
