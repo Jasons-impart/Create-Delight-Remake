b39e891fb662ae40aec54244b5cbea6f2e5dbabb
CPearl <1269596929@qq.com>
2026-01-16 18:18:15 +0800

    [fix]正确限制合金电炉输入，修复某种原料不满时却无法送入的bug (#1394)

--- kubejs/server_scripts/mbd2/alloy_electric_furnace.js
+++ kubejs/server_scripts/mbd2/alloy_electric_furnace.js
@@ -4,16 +4,19 @@ MBDMachineEvents.onStructureFormed("createdelight:alloy_electric_furnace", e => 
     /**@type {ItemStackTransfer} */
     let input = machine.getTraitByName("forged_steel_import_item_slot").storage
     input.setFilter(item => {
-        // console.log(item)
+        // console.log(`filter item: ${item}`)
         let hasItem = false
         let canStack = false
 
         for (let i = 0; i < input.slots; i++) {
             let stack = input.getStackInSlot(i)
+            // console.log(`slot ${i} item: ${stack}`)
             if (ItemTransferHelper.canItemStacksStack(item, stack)) {
                 hasItem = true
-                if (item.count + stack.count <= 64)
+                if (stack.count < 64)
                     canStack = true
+                else
+                    return false
             }
         }
         // console.log(hasItem, canStack)
