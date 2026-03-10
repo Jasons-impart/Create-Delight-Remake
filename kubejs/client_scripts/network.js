ccc485cbbe80911074f2b58ba808085fcf966857
AzureCrab <wanquan.wu.ca@gmail.com>
2026-03-11 00:33:00 +0800

    [fix]修复连锁装壳无限应力的问题

--- kubejs/client_scripts/network.js
+++ kubejs/client_scripts/network.js
@@ -7,7 +7,7 @@ NetworkEvents.dataReceived("kubejs_player_playsound", e => {
 })
 
 NetworkEvents.dataReceived("spawn_create_particle", e => {
-    const {fluid, x, y, z, vx, vy, vz, count, speed} = e.data
+    const { fluid, x, y, z, vx, vy, vz, count, speed } = e.data
     let particle = $FluidFX.getFluidParticle(Fluid.of(fluid).fluidStack)
     e.player.level.spawnParticles(particle, true, x, y, z, vx, vy, vz, count, speed)
 })
@@ -18,13 +18,42 @@ NetworkEvents.dataReceived("openholo", e => {
     gui.onShow()
 })
 
-// 发送玩家是否按下了Alt键，用于 批量套壳 server_scripts/Custom/encase.js
-let playerAltDown = false
-let playerAltDown1 = false
+// 发送玩家是否按下了Alt键并显示状态文本提示，用于 批量套壳 server_scripts/Custom/encase.js
+let isAltDown = false
+let wasAltDown = false
+const Encasable = ['create:shaft', 'create:belt']
+const Uncasingable = ["create:brass_encased_shaft", "create:andesite_encased_shaft", 'create:shaft', 'create:belt']
+const beltCasing = ["create:brass_casing", "create:andesite_casing"]
 PlayerEvents.tick(event => {
-    playerAltDown = Client.isAltDown()
-    if (playerAltDown1 != playerAltDown) {
-        event.player.sendData("isPlayerAltDown", { "Alt": playerAltDown })
-        playerAltDown1 = playerAltDown
+    isAltDown = Client.isAltDown()
+    if (isAltDown != wasAltDown) {
+        event.player.sendData("isPlayerAltDown", { "Alt": isAltDown })
+        wasAltDown = isAltDown
+    }
+
+    if (event.level.time % 10 != 0) { return }
+    let viewBlock = event.player.rayTrace(event.player.getAttributeValue("forge:block_reach") + 1, true).block
+    let Item = event.player.mainHandItem
+    let isCasing = false
+    let isEncasable = false
+    let isUncasingable = false
+
+    if (Item != null) {
+        isCasing = beltCasing.some(i => i == Item.id)
+    }
+    if (viewBlock != null) {
+        isEncasable = Encasable.some(i => i == viewBlock.id)
+        isUncasingable = Uncasingable.some(i => i == viewBlock.id)
+    }
+
+    if (!Client.isAltDown()) {
+        if (isCasing && isEncasable) {
+            event.player.setStatusMessage(Text.translate("message.kubejs.chainedcasing"))
+            return
+        }
+        if ("create:wrench" == Item.id && isUncasingable) {
+            event.player.setStatusMessage(Text.translate("message.kubejs.chaineduncasing"))
+            return
+        }
     }
 })
