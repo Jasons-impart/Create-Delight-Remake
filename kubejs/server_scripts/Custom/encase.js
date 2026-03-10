878881998ef96cf6cb569410f9a292573983e984
AzureCrab <wanquan.wu.ca@gmail.com>
2026-03-11 00:33:59 +0800

    [fix]修复连锁装壳无限应力的问题 (#1497)

--- kubejs/server_scripts/Custom/encase.js
+++ kubejs/server_scripts/Custom/encase.js
@@ -1,24 +1,20 @@
 // Credit to 史莱姆li
 // 还有client部分代码，见 client/network.js
 let $Boolean = Java.loadClass("java.lang.Boolean")
-const beltCasing = ["create:brass_casing", "create:andesite_casing"]
+const beltCasing = ["create:brass_casing", "create:andesite_casing", "create:wrench"]
 
 const shaftCasingType = { "create:brass_casing": "create:brass_encased_shaft", "create:andesite_casing": "create:andesite_encased_shaft" }
-const beltCasingType = { "create:brass_casing": "BRASS", "create:andesite_casing": "ANDESITE" }
+const beltCasingType = { "create:brass_casing": 'BRASS', "create:andesite_casing": "ANDESITE", "create:wrench": 'NONE' }
 const inversedBeltCasingType = { "BRASS": "create:brass_casing", "ANDESITE": "create:andesite_casing" }
+
 
 NetworkEvents.dataReceived("isPlayerAltDown", event => {
     event.player.persistentData.Alt = event.data.Alt
-    if (beltCasing.some(i => i == event.player.mainHandItem.id) && event.data.Alt == true) {
-        event.player.setStatusMessage(Text.translate("message.kubejs.chainedcasing"))
-    }
-    if ("create:wrench" == event.player.mainHandItem.id && event.data.Alt == true) {
-        event.player.setStatusMessage(Text.translate("message.kubejs.chaineduncasing"))
-    }
 })
-
+let $KineticBlockEntity = Java.loadClass("com.simibubi.create.content.kinetics.base.KineticBlockEntity")
 let $BeltBlock = Java.loadClass("com.simibubi.create.content.kinetics.belt.BeltBlock")
-//传送带连锁套壳
+let $BeltBlockEntity = Java.loadClass("com.simibubi.create.content.kinetics.belt.BeltBlockEntity")
+//传送带连锁套拆壳
 BlockEvents.rightClicked("create:belt", event => {
     if (event.player == null) { return }
     if (event.player.isShiftKeyDown()) { return }
@@ -34,6 +30,7 @@ BlockEvents.rightClicked("create:belt", event => {
         let HorizontalFacing = BlockStates.getValue($BeltBlock.HORIZONTAL_FACING).toString()
         let Slope = BlockStates.getValue($BeltBlock.SLOPE).toString()
         let YAxis = false//水平传送带还是斜传送带
+        let YBelt = false//是否是垂直传送带
         let toEndblock = block
         let toEndBlockStates = toEndblock.blockState
         let toEndPos = [0, 0, 0]
@@ -42,40 +39,43 @@ BlockEvents.rightClicked("create:belt", event => {
         let toStartBlockStates = toStartblock.blockState
         let toStartPos = [0, 0, 0]
         let toStartVec = [0, 0, 0]
-        //套？拆？
-        let encaseOrNot = true
-        //let encaseOrNot = !Boolean(BlockStates.getValue($BeltBlock.CASING).toString())
-        //encaseOrNot = encaseOrNot || !(event.item.id == inversedBeltCasingType[block.entityData.Casing])
         //event.player.tell(encaseOrNot)
-        switch (HorizontalFacing) {
-            case 'east':
-                toEndVec[0] = 1
-                toStartVec[0] = -1
-                break
-            case 'west':
-                toEndVec[0] = -1
-                toStartVec[0] = 1
-                break
-            case 'south':
-                toEndVec[2] = 1
-                toStartVec[2] = -1
-                break
-            case 'north':
-                toEndVec[2] = -1
-                toStartVec[2] = 1
-                break
+        if (Slope == 'VERTICAL') { YBelt = true }
+        if (YBelt) {
+            toEndVec[1] = 1
+            toStartVec[1] = -1
         }
-        if (Slope != 'HORIZONTAL') { YAxis = true }
-        if (YAxis) {
-            switch (Slope) {
-                case 'UPWARD':
-                    toEndVec[1] = 1
-                    toStartVec[1] = -1
+        else {
+            switch (HorizontalFacing) {
+                case 'east':
+                    toEndVec[0] = 1
+                    toStartVec[0] = -1
                     break
-                case 'DOWNWARD':
-                    toEndVec[1] = -1
-                    toStartVec[1] = 1
+                case 'west':
+                    toEndVec[0] = -1
+                    toStartVec[0] = 1
                     break
+                case 'south':
+                    toEndVec[2] = 1
+                    toStartVec[2] = -1
+                    break
+                case 'north':
+                    toEndVec[2] = -1
+                    toStartVec[2] = 1
+                    break
+            }
+            if (Slope != 'HORIZONTAL') { YAxis = true }
+            if (YAxis) {
+                switch (Slope) {
+                    case 'UPWARD':
+                        toEndVec[1] = 1
+                        toStartVec[1] = -1
+                        break
+                    case 'DOWNWARD':
+                        toEndVec[1] = -1
+                        toStartVec[1] = 1
+                        break
+                }
             }
         }
         //遍历至末端
@@ -86,11 +86,11 @@ BlockEvents.rightClicked("create:belt", event => {
                 toEndPos[2] += toEndVec[2]
                 toEndblock = block.offset(toEndPos[0], toEndPos[1], toEndPos[2])
                 if (toEndblock.id != 'create:belt') { break }
-                toEndBlockStates = toEndblock.blockState
-                //event.player.tell(toEndblock.pos)
-                event.level.setBlockAndUpdate(toEndblock.pos, toEndBlockStates.setValue($BeltBlock.CASING, encaseOrNot ? $Boolean.TRUE : $Boolean.FALSE))
-                event.level.server.runCommandSilent(`/data modify block ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} Casing set value "${encaseOrNot ? beltCasingType[event.item.id] : 'NONE'}"`)
-                //event.level.blockEntityChanged(toEndPos[0]+block.pos.x,toEndPos[1]+block.pos.y,toEndPos[2]+block.pos.z)
+                if (toEndblock.entity instanceof $BeltBlockEntity) {
+                    toEndblock.entity.setCasingType(beltCasingType[event.item.id])
+                }
+
+
                 //是否继续
                 if (toEndBlockStates.getValue($BeltBlock.PART).toString() == 'END') {
                     break
@@ -105,9 +105,9 @@ BlockEvents.rightClicked("create:belt", event => {
                 toStartPos[2] += toStartVec[2]
                 toStartblock = block.offset(toStartPos[0], toStartPos[1], toStartPos[2])
                 if (toStartblock.id != 'create:belt') { break }
-                toStartBlockStates = toStartblock.blockState
-                event.level.setBlockAndUpdate(toStartblock.pos, toStartBlockStates.setValue($BeltBlock.CASING, encaseOrNot ? $Boolean.TRUE : $Boolean.FALSE))
-                event.level.server.runCommandSilent(`/data modify block ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} Casing set value "${encaseOrNot ? beltCasingType[event.item.id] : 'NONE'}"`)
+                if (toStartblock.entity instanceof $BeltBlockEntity) {
+                    toStartblock.entity.setCasingType(beltCasingType[event.item.id])
+                }
                 //是否继续
                 if (toStartBlockStates.getValue($BeltBlock.PART).toString() == 'START') {
                     break
@@ -165,7 +165,7 @@ BlockEvents.rightClicked("create:shaft", event => {
             if (toEndBlockStates.getValue($ShaftBlock.AXIS).toString() != Axis) {
                 break
             }
-            event.level.server.runCommandSilent(`/setblock ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} ${shaftCasingType[event.item.id]}[axis=${Axis.toLowerCase()}]`)
+            event.level.runCommandSilent(`/setblock ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} ${shaftCasingType[event.item.id]}[axis=${Axis.toLowerCase()}]`)
         }
 
         //遍历至起始端
@@ -177,7 +177,7 @@ BlockEvents.rightClicked("create:shaft", event => {
             if (toStartblock.id != 'create:shaft' && toStartblock.id != shaftCasingType[event.item.id]) { break }
             toStartBlockStates = toStartblock.blockState
             if (toStartBlockStates.getValue($ShaftBlock.AXIS).toString() != Axis) { break }
-            event.level.server.runCommandSilent(`/setblock ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} ${shaftCasingType[event.item.id]}[axis=${Axis.toLowerCase()}]`)
+            event.level.runCommandSilent(`/setblock ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} ${shaftCasingType[event.item.id]}[axis=${Axis.toLowerCase()}]`)
         }
 
     }
@@ -230,7 +230,7 @@ BlockEvents.rightClicked("create:brass_encased_shaft", event => {
         if (toEndBlockStates.getValue($ShaftBlock.AXIS).toString() != Axis) {
             break
         }
-        event.level.server.runCommandSilent(`/setblock ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
+        event.level.runCommandSilent(`/setblock ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
     }
     //遍历至起始端
     for (let i = 0; i < limit; i++) {
@@ -241,7 +241,7 @@ BlockEvents.rightClicked("create:brass_encased_shaft", event => {
         if (toStartblock.id != "create:brass_encased_shaft") { break }
         toStartBlockStates = toStartblock.blockState
         if (toStartBlockStates.getValue($ShaftBlock.AXIS).toString() != Axis) { break }
-        event.level.server.runCommandSilent(`/setblock ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
+        event.level.runCommandSilent(`/setblock ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
     }
 })
 BlockEvents.rightClicked("create:andesite_encased_shaft", event => {
@@ -291,7 +291,7 @@ BlockEvents.rightClicked("create:andesite_encased_shaft", event => {
         if (toEndBlockStates.getValue($ShaftBlock.AXIS).toString() != Axis) {
             break
         }
-        event.level.server.runCommandSilent(`/setblock ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
+        event.level.runCommandSilent(`/setblock ${toEndblock.pos.x} ${toEndblock.pos.y} ${toEndblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
     }
     //遍历至起始端
     for (let i = 0; i < limit; i++) {
@@ -302,6 +302,6 @@ BlockEvents.rightClicked("create:andesite_encased_shaft", event => {
         if (toStartblock.id != "create:andesite_encased_shaft") { break }
         toStartBlockStates = toStartblock.blockState
         if (toStartBlockStates.getValue($ShaftBlock.AXIS).toString() != Axis) { break }
-        event.level.server.runCommandSilent(`/setblock ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
+        event.level.runCommandSilent(`/setblock ${toStartblock.pos.x} ${toStartblock.pos.y} ${toStartblock.pos.z} create:shaft[axis=${Axis.toLowerCase()}]`)
     }
 })
