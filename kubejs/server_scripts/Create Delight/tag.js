6f5d63e5742ec913893c9cd0d1ba9076bf4c6a31
AzureCrab <wanquan.wu.ca@gmail.com>
2026-03-28 01:27:17 +0800

    [fix] 为锻造钢板添加 forge:plates 标签

--- kubejs/server_scripts/Create Delight/tag.js
+++ kubejs/server_scripts/Create Delight/tag.js
@@ -2,3 +2,8 @@ ServerEvents.tags("minecraft:block", e => {
     e.add("minecraft:dirt", ['createdelightcore:luna_soil', 'createdelightcore:phantom_compost'])
     e.add("minecraft:mushroom_grow_block", ['createdelightcore:luna_soil', 'createdelightcore:phantom_compost'])
 })
+
+ServerEvents.tags("minecraft:item", e => {
+    // 锻造钢板 - 添加到 forge:plates 标签
+    e.add("forge:plates", ['createdelight:forged_steel_sheet'])
+})
