693c7063f233f58daba9c7d44614d819941ee523
SSWTLZZ69 <1824914068@qq.com>
2026-03-28 18:03:16 +0800

    Revert "[mod]添加流体包裹模组，并修改相应配方 (#1506)" (#1546)
    This reverts commit d890654fa5c64902588e7ce3d46b9c9fd5023fd3.

--- kubejs/server_scripts/Fluid Logistics/recipe.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(e => {
-    remove_recipes_id(e, [
-        "fluidlogistics:multi_fluid_tank",
-        "fluidlogistics:horizontal_multi_fluid_tank"
-    ])
-    e.replaceInput({ id: "fluidlogistics:fluid_packager" }, "minecraft:copper_ingot", "create:copper_sheet")
-})
