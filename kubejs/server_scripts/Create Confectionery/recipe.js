4a0f2311140cf4c07b8313bf47f25038bee9a994
KazeShukufuku <13942976035@163.com>
2026-03-29 22:21:26 +0800

    [mod & fix]添加createidlx（显示链接器加强），并添加汉化补丁；修改“xxx包层巧克力棒”的注液配方

--- kubejs/server_scripts/Create Confectionery/recipe.js
+++ kubejs/server_scripts/Create Confectionery/recipe.js
@@ -1,4 +1,7 @@
 ServerEvents.recipes(e => {
+    e.replaceInput({id: "create_confectionery:black_chocolate_candy_1_recipe"}, "#forge:bars/chocolate", "create:bar_of_chocolate")
+    e.replaceInput({id: "create_confectionery:white_chocolate_candy_1_recipe"}, "#forge:bars/chocolate", "create:bar_of_chocolate")
+    e.replaceInput({id: "create_confectionery:ruby_chocolate_candy_1_recipe"}, "#forge:bars/chocolate", "create:bar_of_chocolate")
     const {create} = e.recipes
     create.mixing(
         "2x create_confectionery:honey_candy",
