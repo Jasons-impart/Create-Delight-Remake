28f6f4890a1b3277c616e6c773692cccbec415c3
KazeShukufuku <122512130+KazeShukufuku@users.noreply.github.com>
2026-03-31 22:55:24 +0800

    [fix & mod]修改JEI.js，使鼓风机冷冻触媒在JEI中能被搜索到；更新懒惰刻修复#1492 (#1551)
    * [fix & mod]修改JEI.js，使鼓风机冷冻触媒在JEI中能被搜索到；更新懒惰刻
    
    * 修改冷冻触媒加载的判定条件
    
    * 重新开启分液池优化

--- kubejs/client_scripts/JEI.js
+++ kubejs/client_scripts/JEI.js
@@ -1,8 +1,20 @@
-JEIEvents.addItems(e => {
-  e.add([
-    'create_connected:fan_freezing_catalyst'
-  ])
+const $FeatureToggle = Java.loadClass('com.hlysine.create_connected.config.FeatureToggle')
+const $ResourceLocation = Java.loadClass('net.minecraft.resources.ResourceLocation')
+
+const forcedJeiItems = [
+  'create_connected:fan_freezing_catalyst'
+]
+
+// Create Connected 给该触媒加了兼容模组条件，进入世界后刷新可见性时会把它判成 disabled。
+// 当前整合包并没有这些兼容模组，直接移除条件即可避免 JEI 和创造搜索被再次过滤。
+forcedJeiItems.forEach(id => {
+  $FeatureToggle.FEATURE_CONDITIONS.remove(new $ResourceLocation(id))
 })
+
+JEIEvents.addItems(e => {
+  e.add(forcedJeiItems)
+})
+
 JEIEvents.addFluids(e => {
   e.add([
     "minecraft:milk"
