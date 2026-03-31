d55f2bb533e2258911992f85fbb058a874980cf2
KazeShukufuku <13942976035@163.com>
2026-03-31 19:09:40 +0800

    [fix & mod]修改JEI.js，使鼓风机冷冻触媒在JEI中能被搜索到；更新懒惰刻

--- kubejs/client_scripts/JEI.js
+++ kubejs/client_scripts/JEI.js
@@ -1,8 +1,53 @@
+const $ArrayList = Java.loadClass('java.util.ArrayList')
+const $Minecraft = Java.loadClass('net.minecraft.client.Minecraft')
+const $VanillaTypes = Java.loadClass('mezz.jei.api.constants.VanillaTypes')
+const $CreateConnectedJEI = Java.loadClass('com.hlysine.create_connected.compat.CreateConnectedJEI')
+
+const forcedJeiItems = [
+  'create_connected:fan_freezing_catalyst'
+]
+
+function syncForcedJeiItems() {
+  const manager = $CreateConnectedJEI.MANAGER
+  if (manager == null) return false
+
+  const stacks = new $ArrayList()
+  forcedJeiItems.forEach(id => stacks.add(Item.of(id)))
+
+  manager.removeIngredientsAtRuntime($VanillaTypes.ITEM_STACK, stacks)
+  manager.addIngredientsAtRuntime($VanillaTypes.ITEM_STACK, stacks)
+  return true
+}
+
 JEIEvents.addItems(e => {
-  e.add([
-    'create_connected:fan_freezing_catalyst'
-  ])
+  e.add(forcedJeiItems)
 })
+
+let jeiSyncCooldown = 0
+let jeiSyncDone = false
+let jeiLastLevel = null
+
+ClientEvents.tick(() => {
+  const level = $Minecraft.getInstance().level
+
+  if (level !== jeiLastLevel) {
+    jeiLastLevel = level
+    jeiSyncDone = false
+    jeiSyncCooldown = level == null ? 0 : 20
+  }
+
+  if (level == null || jeiSyncDone) return
+
+  if (jeiSyncCooldown > 0) {
+    jeiSyncCooldown--
+    return
+  }
+
+  // 每次进存档后重新补一次；JEI runtime 未就绪时短周期重试，成功后停止。
+  jeiSyncDone = syncForcedJeiItems()
+  jeiSyncCooldown = jeiSyncDone ? 0 : 20
+})
+
 JEIEvents.addFluids(e => {
   e.add([
     "minecraft:milk"
