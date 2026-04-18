22d5e68080503e59aafb1322668055b434bdc2e0
AzureCrab <wanquan.wu.ca@gmail.com>
2026-04-19 00:23:03 +0800

    [fix] 修复河豚水饺配方物品ID错误 (#1582)
    * [mod] 更新节日佳肴(Festival Delicacies)至2.0.0-beta；添加饺子乐事(Dumplings Delight) mod
    
    * [fix] 修复河豚水饺配方物品ID错误，删除debug日志

--- kubejs/server_scripts/Festival Delicacies/compat.js
+++ kubejs/server_scripts/Festival Delicacies/compat.js
@@ -1,4 +1,17 @@
 ServerEvents.recipes(e => {
+    e.remove({ id: "festival_delicacies:fd_cooking/meat_zongzi_fd_cooking" })
+    e.recipes.farmersdelight.cooking(
+        [
+            "#forge:crops/rice",
+            "festival_delicacies:preserved_meat",
+            "festival_delicacies:bamboo_leaves",
+            "festival_delicacies:bamboo_leaves"
+        ],
+        "2x festival_delicacies:meat_zongzi",
+        1.0,
+        200
+    ).id("festival_delicacies:fd_cooking/meat_zongzi_fd_cooking")
+
     cutting(e, "festival_delicacies:chinese_cabbage", [
         ["festival_delicacies:chinese_cabbage_leaf", 2],
         ["festival_delicacies:chinese_cabbage_leaf", 1, 0.3]
@@ -11,25 +24,25 @@ ServerEvents.recipes(e => {
         'festival_delicacies:red_bean_paste',
         1.0, 200,
         "minecraft:bowl"
-    ).id("festival_delicacies:red_bean_paste")
+    ).id("createdelight:red_bean_paste")
     e.recipes.create.mixing(
         'festival_delicacies:red_bean_paste',
         [
             '2x neapolitan:roasted_adzuki_beans',
             "minecraft:bowl"
         ]
-    ).heated().id("festival_delicacies:mixing/red_bean_paste")
-    threshing(e, 'festival_delicacies:artemisia_argyi', [
+    ).heated().id("createdelight:mixing/red_bean_paste")
+    threshing(e, 'festival_delicacies:mugwort', [
         'createdelight:artemisia_argyi_seed',
         Item.of('createdelight:artemisia_argyi_seed').withChance(0.5)
     ], 200)
     e.recipes.farmersdelight.cooking(
         [
-            'festival_delicacies:artemisia_argyi',
+            'festival_delicacies:mugwort',
             'festival_delicacies:red_bean_paste',
             'farmersdelight:rice'
         ],
         'festival_delicacies:qingtuan',
         1.0, 200,
-    ).id("festival_delicacies:farmersdelight/qingtuan")
+    ).id("createdelight:farmersdelight/qingtuan")
 })
