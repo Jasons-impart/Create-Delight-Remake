a96cf1a610c87e21cf344cc2adde5afea7e19973
SSWTLZZ69 <1824914068@qq.com>
2026-05-07 19:48:41 +0800

    加入神化，并只启用刷怪笼模块；加入jei tetra和 create wizardry

---
+++ kubejs/server_scripts/Feywild/biome_tags.js
@@ -0,0 +1,59 @@
+ServerEvents.tags("minecraft:worldgen/biome", e => {
+    e.add("kubejs:feywild/autumn_pixie_spawns", [
+        "minecraft:forest",
+        "minecraft:birch_forest",
+        "minecraft:dark_forest",
+        "minecraft:old_growth_birch_forest",
+        "minecraft:old_growth_pine_taiga",
+        "minecraft:flower_forest",
+        "terralith:birch_taiga",
+        "terralith:forested_highlands",
+        "terralith:lavender_forest",
+        "terralith:temperate_highlands",
+        "terralith:yosemite_lowlands",
+        "terralith:skylands_autumn"
+    ])
+
+    e.add("kubejs:feywild/spring_pixie_spawns", [
+        "minecraft:plains",
+        "minecraft:sunflower_plains",
+        "minecraft:meadow",
+        "terralith:blooming_valley",
+        "terralith:blooming_plateau",
+        "terralith:valley_clearing",
+        "terralith:sakura_valley",
+        "terralith:skylands_spring"
+    ])
+
+    e.add("kubejs:feywild/summer_pixie_spawns", [
+        "minecraft:desert",
+        "minecraft:savanna",
+        "minecraft:savanna_plateau",
+        "minecraft:badlands",
+        "minecraft:eroded_badlands",
+        "terralith:ashen_savanna",
+        "terralith:fractured_savanna",
+        "terralith:savanna_slopes",
+        "terralith:hot_shrubland",
+        "terralith:desert_oasis",
+        "terralith:desert_spires",
+        "terralith:savanna_badlands",
+        "terralith:bryce_canyon",
+        "terralith:skylands_summer"
+    ])
+
+    e.add("kubejs:feywild/winter_pixie_spawns", [
+        "minecraft:ice_spikes",
+        "minecraft:snowy_taiga",
+        "minecraft:snowy_plains",
+        "minecraft:snowy_slopes",
+        "terralith:wintry_forest",
+        "terralith:wintry_lowlands",
+        "terralith:snowy_cherry_grove",
+        "terralith:snowy_maple_forest",
+        "terralith:snowy_badlands",
+        "terralith:snowy_shield",
+        "terralith:frozen_cliffs",
+        "terralith:skylands_winter"
+    ])
+})
