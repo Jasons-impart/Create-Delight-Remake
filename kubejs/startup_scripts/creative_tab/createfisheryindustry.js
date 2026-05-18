fb7115e2c7e9706e470c1aad474437b5f3887107
Kaze <122512130+KazeShukufuku@users.noreply.github.com>
2026-05-18 12:40:55 +0800

    [mod] 移除机械动力：渔业及其相关修改 (#1659)

--- kubejs/startup_scripts/creative_tab/createfisheryindustry.js
+++
@@ -1,6 +0,0 @@
-StartupEvents.modifyCreativeTab("createfisheryindustry:fishery_equipment_tab", e => {
-  e.remove([
-    'createfisheryindustry:pneumatic_harpoon_gun',
-    'createfisheryindustry:pneumatic_mechanism'
-  ])
-})
