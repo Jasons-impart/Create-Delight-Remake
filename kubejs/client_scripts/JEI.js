11f6db76a7a411837f8aeca8c9be2f5985eaaad8
prithief <99400939+prithief@users.noreply.github.com>
2026-07-16 16:41:28 +0800

    禁用大部分mmt材料 (#1982)

--- kubejs/client_scripts/JEI.js
+++ kubejs/client_scripts/JEI.js
@@ -31,5 +31,28 @@ JEIEvents.hideItems(e => {
     'createdeco:industrial_iron_coinstack',
     'createdeco:zinc_coin',
     'createdeco:zinc_coinstack',
+
+    // MMT 禁用材料 - JEI 隐藏
+    'more_mod_tetra:fake_rose_golden_silk',
+    'more_mod_tetra:fake_silky_cloth',
+    'more_mod_tetra:bone_ingot',
+    'more_mod_tetra:tidal_ingot',
+    'more_mod_tetra:sacrifice_stone',
+    'more_mod_tetra:bokushuu_ingot',
+    'more_mod_tetra:bokuka_ingot',
+    'more_mod_tetra:rennshi_ingot',
+    'more_mod_tetra:blaze_ingot',
+    'more_mod_tetra:blue_ice_ingot',
+    'more_mod_tetra:electro_charged_ingot',
+    'more_mod_tetra:dragon_breath_ingot',
+    'more_mod_tetra:fake_alumite_ingot',
+    'more_mod_tetra:fake_manyullyn_ingot',
+    'more_mod_tetra:fake_hepatizon_ingot',
+    'more_mod_tetra:fake_rose_gold_ingot',
+    'more_mod_tetra:fake_pig_iron_ingot',
+    'more_mod_tetra:abyssal_ocean_ingot',
+    'more_mod_tetra:wither_ingot',
+    'more_mod_tetra:rotten_flesh_ingot',
+    'more_mod_tetra:wither_bone_ingot',
   ])
 })
