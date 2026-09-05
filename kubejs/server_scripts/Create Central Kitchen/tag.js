81adcef5f15816dc75bbe13c00bb2b6b731af605
Kaze <122512130+KazeShukufuku@users.noreply.github.com>
2026-09-05 16:51:37 +0800

    [feat] 合并中央厨房和龙+的龙息流体；移除重复的附魔工业文件 (#2235)

---
+++ kubejs/server_scripts/Create Central Kitchen/tag.js
@@ -0,0 +1,7 @@
+ServerEvents.tags("fluid", e => {
+    e.add("quality_food_fluids:quality_fluids", [
+        "create_dragons_plus:dragon_breath",
+        "create_dragons_plus:flowing_dragon_breath"
+    ])
+    e.add("c:hidden_from_recipe_viewers", "create_central_kitchen:dragon_breath")
+})
--- kubejs/server_scripts/Ice and Fire/recipes.js
+++ kubejs/server_scripts/Ice and Fire/recipes.js
@@ -23,7 +23,7 @@ ServerEvents.recipes(e => {
         "iceandfire:dragonsteel_lightning_boots",
         "iceandfire:copper_pile"
     ])
-    create.filling("ends_delight:raw_dragon_meat", ["#createdelight:dragon_flesh", Fluid.of("create_central_kitchen:dragon_breath", 250)])
+    create.filling("ends_delight:raw_dragon_meat", ["#createdelight:dragon_flesh", Fluid.of("create_dragons_plus:dragon_breath", 250)])
     e.replaceInput({ id: "iceandfire:dragon_meal" }, "#iceandfire:dragon_food_meat", "#forge:meat/raw")
     create.haunting(Item.of("iceandfire:rotten_egg").withChance(0.25), "minecraft:egg").id("createdelight:haunting/rotten_egg")
     iceandfire.dragonforge(
