75e9ba951cb62d492ab4ac1c187d98e01543955d
KazeShukufuku <13942976035@163.com>
2026-09-05 17:07:55 +0800

    [mod] 更新流体包裹，初步修改相关文件

---
+++ kubejs/server_scripts/Create Fluid Logistics/tag.js
@@ -0,0 +1,3 @@
+ServerEvents.tags("fluid", e => {
+    e.add("c:hidden_from_recipe_viewers", "fluidlogistics:powder_snow")
+})
--- kubejs/server_scripts/Create More Recipe/recipe.js
+++ kubejs/server_scripts/Create More Recipe/recipe.js
@@ -4,7 +4,7 @@ ServerEvents.recipes(e => {
         "cmr:frozen_cake_base",
         "cmr:frozen_cake"
     ])
-    create.compacting("4x cmr:frozen_cake_base", [
+    /* create.compacting("4x cmr:frozen_cake_base", [
         Fluid.of("createdelight:cake_batter", 1000),
         "2x minecraft:snowball"
     ])
@@ -12,7 +12,7 @@ ServerEvents.recipes(e => {
     // .heatRequirement("cooled")
     .id("createdelight:compacting/frozen_cake_base")
     create.filling("cmr:frozen_cake", ["cmr:frozen_cake_base", Fluid.of("netherexp:ectoplasm", 250)])
-    .id("createdelight:filling/frozen_cake")
+    .id("createdelight:filling/frozen_cake") */
     kubejs.shaped("2x cmr:empty_snowman_cooler", [
         " A ",
         "ABA",
